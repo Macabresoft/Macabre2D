c26f507d5aff06199916843eb47d58330b08b3ed
BrettStory <brettmstory@gmail.com>
2019-02-19 19:42:54 -0600

    Feature: adding a red and green square to represent a grabber for the translate gizmo.

--- UI/Controls/SceneEditing/TranslateGizmo.cs
+++ UI/Controls/SceneEditing/TranslateGizmo.cs
@@ -7,8 +7,10 @@
     using Microsoft.Xna.Framework.Input;
 
     public sealed class TranslateGizmo : BaseGizmo {
-        private readonly SpriteRenderer _xAxisSpriteRenderer = new SpriteRenderer();
-        private readonly SpriteRenderer _yAxisSpriteRenderer = new SpriteRenderer();
+        private readonly SpriteRenderer _xAxisArrowRenderer = new SpriteRenderer();
+        private readonly SpriteRenderer _xAxisTriangleRenderer = new SpriteRenderer();
+        private readonly SpriteRenderer _yAxisArrowRenderer = new SpriteRenderer();
+        private readonly SpriteRenderer _yAxisTriangleRenderer = new SpriteRenderer();
 
         public TranslateGizmo(EditorGame editorGame) : base(editorGame) {
         }
@@ -16,13 +18,21 @@
         public override void Initialize() {
             base.Initialize();
 
-            var arrowSprite = PrimitiveDrawer.CreateArrowSprite(this.EditorGame.GraphicsDevice, 32);
-            this._xAxisSpriteRenderer.Sprite = arrowSprite;
-            this._yAxisSpriteRenderer.Sprite = arrowSprite;
-            this._xAxisSpriteRenderer.OffsetType = OffsetType.Center;
-            this._yAxisSpriteRenderer.OffsetType = OffsetType.Center;
-            this._xAxisSpriteRenderer.Initialize(this.EditorGame.CurrentScene);
-            this._yAxisSpriteRenderer.Initialize(this.EditorGame.CurrentScene);
+            var arrowSprite = PrimitiveDrawer.CreateArrowSprite(this.EditorGame.GraphicsDevice, 64);
+            this._xAxisArrowRenderer.Sprite = arrowSprite;
+            this._yAxisArrowRenderer.Sprite = arrowSprite;
+            this._xAxisArrowRenderer.OffsetType = OffsetType.Center;
+            this._yAxisArrowRenderer.OffsetType = OffsetType.Center;
+            this._xAxisArrowRenderer.Initialize(this.EditorGame.CurrentScene);
+            this._yAxisArrowRenderer.Initialize(this.EditorGame.CurrentScene);
+
+            var triangleSprite = PrimitiveDrawer.CreateRightTriangleSprite(this.EditorGame.GraphicsDevice, new Point(64));
+            this._xAxisTriangleRenderer.Sprite = triangleSprite;
+            this._yAxisTriangleRenderer.Sprite = triangleSprite;
+            this._xAxisTriangleRenderer.OffsetType = OffsetType.Center;
+            this._yAxisTriangleRenderer.OffsetType = OffsetType.Center;
+            this._xAxisTriangleRenderer.Initialize(this.EditorGame.CurrentScene);
+            this._yAxisTriangleRenderer.Initialize(this.EditorGame.CurrentScene);
         }
 
         public override void Update(GameTime gameTime, MouseState mouseState, BaseComponent selectedComponent) {
@@ -31,18 +41,30 @@
         }
 
         protected override void DrawGizmo(GameTime gameTime, Transform worldTransform, float viewHeight, float viewRatio) {
-            var scale = viewRatio * 0.5f;
-            this._xAxisSpriteRenderer.Color = this.XAxisColor;
-            this._xAxisSpriteRenderer.LocalPosition = this.XAxisLineDrawer.EndPoint;
-            this._xAxisSpriteRenderer.LocalScale = new Vector2(scale);
-            this._xAxisSpriteRenderer.LocalRotation.Angle = worldTransform.Rotation.Angle - MathHelper.ToRadians(90f);
-            this._xAxisSpriteRenderer.Draw(gameTime, viewHeight);
+            var scale = viewRatio * 0.25f;
+            this._xAxisArrowRenderer.Color = this.XAxisColor;
+            this._xAxisArrowRenderer.LocalPosition = this.XAxisLineDrawer.EndPoint;
+            this._xAxisArrowRenderer.LocalScale = new Vector2(scale);
+            this._xAxisArrowRenderer.LocalRotation.Angle = worldTransform.Rotation.Angle - MathHelper.ToRadians(90f);
+            this._xAxisArrowRenderer.Draw(gameTime, viewHeight);
 
-            this._yAxisSpriteRenderer.Color = this.YAxisColor;
-            this._yAxisSpriteRenderer.LocalPosition = this.YAxisLineDrawer.EndPoint;
-            this._yAxisSpriteRenderer.LocalScale = new Vector2(scale);
-            this._yAxisSpriteRenderer.LocalRotation.Angle = worldTransform.Rotation.Angle;
-            this._yAxisSpriteRenderer.Draw(gameTime, viewHeight);
+            this._yAxisArrowRenderer.Color = this.YAxisColor;
+            this._yAxisArrowRenderer.LocalPosition = this.YAxisLineDrawer.EndPoint;
+            this._yAxisArrowRenderer.LocalScale = new Vector2(scale);
+            this._yAxisArrowRenderer.LocalRotation.Angle = worldTransform.Rotation.Angle;
+            this._yAxisArrowRenderer.Draw(gameTime, viewHeight);
+
+            this._xAxisTriangleRenderer.Color = this.XAxisColor;
+            this._xAxisTriangleRenderer.LocalPosition = this.XAxisLineDrawer.StartPoint;
+            this._xAxisTriangleRenderer.LocalScale = new Vector2(scale);
+            this._xAxisTriangleRenderer.LocalRotation.Angle = worldTransform.Rotation.Angle + MathHelper.ToRadians(180f);
+            this._xAxisTriangleRenderer.Draw(gameTime, viewHeight);
+
+            this._yAxisTriangleRenderer.Color = this.YAxisColor;
+            this._yAxisTriangleRenderer.LocalPosition = this.YAxisLineDrawer.StartPoint;
+            this._yAxisTriangleRenderer.LocalScale = new Vector2(scale);
+            this._yAxisTriangleRenderer.LocalRotation.Angle = worldTransform.Rotation.Angle;
+            this._yAxisTriangleRenderer.Draw(gameTime, viewHeight);
         }
     }
 }
