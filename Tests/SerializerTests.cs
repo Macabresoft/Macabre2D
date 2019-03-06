5e0536d8b2e0cf5a25aa41d0d37afade35dcac27
BrettStory <brettmstory@gmail.com>
2019-03-06 16:59:18 -0600

    Testing: Serializer now tested when serializing and deserializing GameSettings.

---
+++ Tests/SerializerTests.cs
@@ -0,0 +1,47 @@
+﻿namespace Macabre2D.Tests {
+
+    using Macabre2D.Framework;
+    using Microsoft.Xna.Framework;
+    using NUnit.Framework;
+    using System.IO;
+
+    [TestFixture]
+    public static class SerializerTests {
+
+        [Test]
+        [Category("Unit Test")]
+        public static void Serializer_GameSettingsSerializeTest() {
+            var gameSettings = new GameSettings() {
+                FallbackBackgroundColor = Color.Coral,
+                PixelsPerUnit = 200,
+                StartupScenePath = "ThisSceneIsAHoldUp"
+            };
+
+            var fileLocation = "GameSettingsForTest.m2dgs";
+            GameSettings deserializedGameSettings = null;
+            var serializer = new Serializer();
+            try {
+                serializer.Serialize(gameSettings, fileLocation);
+                deserializedGameSettings = serializer.Deserialize<GameSettings>(fileLocation);
+            }
+            finally {
+                File.Delete(fileLocation);
+            }
+
+            Assert.NotNull(deserializedGameSettings);
+            Assert.AreEqual(gameSettings.FallbackBackgroundColor, deserializedGameSettings.FallbackBackgroundColor);
+            Assert.AreEqual(gameSettings.PixelsPerUnit, deserializedGameSettings.PixelsPerUnit);
+            Assert.AreEqual(gameSettings.StartupScenePath, deserializedGameSettings.StartupScenePath);
+
+            deserializedGameSettings = null;
+
+            var gameSettingsString = serializer.SerializeToString(gameSettings);
+            deserializedGameSettings = serializer.DeserializeFromString<GameSettings>(gameSettingsString);
+
+            Assert.NotNull(deserializedGameSettings);
+            Assert.AreEqual(gameSettings.FallbackBackgroundColor, deserializedGameSettings.FallbackBackgroundColor);
+            Assert.AreEqual(gameSettings.PixelsPerUnit, deserializedGameSettings.PixelsPerUnit);
+            Assert.AreEqual(gameSettings.StartupScenePath, deserializedGameSettings.StartupScenePath);
+        }
+    }
+}
