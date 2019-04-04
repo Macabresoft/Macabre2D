833ae9ab95468ab45a007200ee1adc0047b6596e
BrettStory <brettmstory@gmail.com>
2019-04-03 19:54:42 -0500

    Refactor: cleaning up unused fields.

--- UI/ViewModels/ComponentsViewModel.cs
+++ UI/ViewModels/ComponentsViewModel.cs
@@ -1,14 +1,12 @@
-﻿namespace Macabre2D.UI.ViewModels {
-
+﻿namespace Macabre2D.UI.ViewModels
+{
     using Macabre2D.Framework;
-    using Macabre2D.UI.Models.FrameworkWrappers;
     using Macabre2D.UI.ServiceInterfaces;
 
-    public sealed class ComponentsViewModel : NotifyPropertyChanged {
-        private readonly IComponentSelectionService _selectionService;
-        private ComponentWrapper _selectedComponent;
-
-        public ComponentsViewModel(ISceneService sceneService, IComponentSelectionService selectionService) {
+    public sealed class ComponentsViewModel : NotifyPropertyChanged
+    {
+        public ComponentsViewModel(ISceneService sceneService, IComponentSelectionService selectionService)
+        {
             this.SceneService = sceneService;
             this.SelectionService = selectionService;
         }
