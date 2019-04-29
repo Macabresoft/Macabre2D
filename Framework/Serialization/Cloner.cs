2c5d0ef9d745e6ebcf4ef96fd1eb4531505492ce
BrettStory <brettmstory@gmail.com>
2019-04-29 17:49:59 -0500

    Feature: can clone a component from code.

---
+++ Framework/Serialization/Cloner.cs
@@ -0,0 +1,37 @@
+﻿namespace Macabre2D.Framework.Serialization {
+
+    using System;
+
+    public static class Cloner {
+        private static ISerializer _serializer = new Serializer();
+
+        public static BaseComponent Clone<T>(this T component) where T : BaseComponent {
+            T clone = null;
+            if (component != null) {
+                var parent = component.Parent;
+                try {
+                    component.Parent = null;
+                    var json = Cloner._serializer.SerializeToString(component);
+                    clone = Cloner._serializer.DeserializeFromString<T>(json);
+                    var cloneParent = clone.Parent;
+                    clone.SetNewComponentIds();
+                    clone.Parent = parent;
+                }
+                finally {
+                    component.Parent = parent;
+                }
+            }
+
+            return clone;
+        }
+
+        private static void SetNewComponentIds(this BaseComponent component) {
+            if (component != null) {
+                component.Id = Guid.NewGuid();
+                foreach (var child in component.Children) {
+                    child.SetNewComponentIds();
+                }
+            }
+        }
+    }
+}
--- Tests/BaseComponentTests.cs
+++ Tests/BaseComponentTests.cs
@@ -1,12 +1,81 @@
 ﻿namespace Macabre2D.Tests {
 
     using Macabre2D.Framework;
+    using Macabre2D.Framework.Serialization;
+    using Microsoft.Xna.Framework;
     using NSubstitute;
     using NUnit.Framework;
     using System;
+    using System.Linq;
 
     [TestFixture]
     public static class BaseComponentTests {
+
+        [Test]
+        [Category("Unit Test")]
+        public static void BaseComponent_Clone_HasChildTest() {
+            using (var component = new TestComponent())
+            using (var child = new TestComponent()) {
+                component.Initialize(Substitute.For<IScene>());
+
+                var random = new Random();
+                child.LocalPosition = new Vector2(random.Next(-1000, 1000) / 3f, random.Next(-1000, 1000) / 3f);
+                child.LocalScale = new Vector2(random.Next(-1000, 1000) / 3f, random.Next(-1000, 1000) / 3f);
+                child.LocalRotation.Angle = random.Next(0, 10) / 3f;
+
+                component.AddChild(child);
+
+                using (var clone = component.Clone()) {
+                    Assert.AreNotEqual(component.Id, clone.Id);
+
+                    var cloneChild = clone.Children.First();
+                    Assert.AreEqual(child.LocalPosition, cloneChild.LocalPosition);
+                    Assert.AreEqual(child.LocalScale, cloneChild.LocalScale);
+                    Assert.AreEqual(child.LocalRotation.Angle, cloneChild.LocalRotation.Angle);
+                    Assert.AreNotEqual(child.Id, cloneChild.Id);
+                }
+            }
+        }
+
+        [Test]
+        [Category("Unit Test")]
+        public static void BaseComponent_Clone_HasParentTest() {
+            using (var parent = new TestComponent())
+            using (var component = new TestComponent()) {
+                parent.Initialize(Substitute.For<IScene>());
+
+                var random = new Random();
+                component.LocalPosition = new Vector2(random.Next(-1000, 1000) / 3f, random.Next(-1000, 1000) / 3f);
+                component.LocalScale = new Vector2(random.Next(-1000, 1000) / 3f, random.Next(-1000, 1000) / 3f);
+                component.LocalRotation.Angle = random.Next(0, 10) / 3f;
+
+                parent.AddChild(component);
+
+                using (var clone = component.Clone()) {
+                    Assert.AreNotEqual(component.Id, clone.Id);
+                    Assert.NotNull(clone.Parent);
+                    Assert.AreEqual(component.Parent.Id, clone.Parent.Id);
+                }
+            }
+        }
+
+        [Test]
+        [Category("Unit Test")]
+        public static void BaseComponent_Clone_NoParentOrChildrenTest() {
+            using (var component = new TestComponent()) {
+                var random = new Random();
+                component.LocalPosition = new Vector2(random.Next(-1000, 1000) / 3f, random.Next(-1000, 1000) / 3f);
+                component.LocalScale = new Vector2(random.Next(-1000, 1000) / 3f, random.Next(-1000, 1000) / 3f);
+                component.LocalRotation.Angle = random.Next(0, 10) / 3f;
+
+                using (var clone = component.Clone()) {
+                    Assert.AreNotEqual(component.Id, clone.Id);
+                    Assert.AreEqual(component.LocalPosition, clone.LocalPosition);
+                    Assert.AreEqual(component.LocalScale, clone.LocalScale);
+                    Assert.AreEqual(component.LocalRotation.Angle, clone.LocalRotation.Angle);
+                }
+            }
+        }
 
         [Test]
         [Category("Unit Test")]
