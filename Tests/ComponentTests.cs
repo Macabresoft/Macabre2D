1e54a937f7259780d8508107e399e330538f259a
BrettStory <brettmstory@gmail.com>
2019-05-01 18:16:55 -0500

    Refactor: consolidating the two separate test classes for components. This was never fixed from when GameObjects and Components were separate.

--- Tests/ComponentTests.cs
+++ Tests/ComponentTests.cs
@@ -1,14 +1,133 @@
 ﻿namespace Macabre2D.Tests {
 
+    using Macabre2D.Framework;
     using Macabre2D.Framework.Physics;
     using Macabre2D.Framework.Rendering;
+    using Macabre2D.Framework.Serialization;
     using Microsoft.Xna.Framework;
+    using NSubstitute;
     using NUnit.Framework;
+    using System;
     using System.Collections.Generic;
     using System.Linq;
 
     [TestFixture]
     public static class ComponentTests {
+
+        [Test]
+        [Category("Unit Test")]
+        public static void Component_Clone_HasChildTest() {
+            using (var component = new TestComponent())
+            using (var child = new TestComponent()) {
+                component.Initialize(Substitute.For<IScene>());
+
+                var random = new Random();
+                child.LocalPosition = new Vector2(random.Next(-1000, 1000) / 3f, random.Next(-1000, 1000) / 3f);
+                child.LocalScale = new Vector2(random.Next(-1000, 1000) / 3f, random.Next(-1000, 1000) / 3f);
+                child.LocalRotation.Angle = random.Next(0, 10) / 3f;
+
+                component.AddChild(child);
+
+                using (var clone = component.Clone()) {
+                    Assert.AreNotEqual(component.Id, clone.Id);
+
+                    var cloneChild = clone.Children.First();
+                    Assert.AreEqual(child.LocalPosition, cloneChild.LocalPosition);
+                    Assert.AreEqual(child.LocalScale, cloneChild.LocalScale);
+                    Assert.AreEqual(child.LocalRotation.Angle, cloneChild.LocalRotation.Angle);
+                    Assert.AreNotEqual(child.Id, cloneChild.Id);
+                }
+            }
+        }
+
+        [Test]
+        [Category("Unit Test")]
+        public static void Component_Clone_HasParentTest() {
+            using (var parent = new TestComponent())
+            using (var component = new TestComponent()) {
+                parent.Initialize(Substitute.For<IScene>());
+
+                var random = new Random();
+                component.LocalPosition = new Vector2(random.Next(-1000, 1000) / 3f, random.Next(-1000, 1000) / 3f);
+                component.LocalScale = new Vector2(random.Next(-1000, 1000) / 3f, random.Next(-1000, 1000) / 3f);
+                component.LocalRotation.Angle = random.Next(0, 10) / 3f;
+
+                parent.AddChild(component);
+
+                using (var clone = component.Clone()) {
+                    Assert.AreNotEqual(component.Id, clone.Id);
+                    Assert.NotNull(clone.Parent);
+                    Assert.AreEqual(component.Parent.Id, clone.Parent.Id);
+                }
+            }
+        }
+
+        [Test]
+        [Category("Unit Test")]
+        public static void Component_Clone_NoParentOrChildrenTest() {
+            using (var component = new TestComponent()) {
+                var random = new Random();
+                component.LocalPosition = new Vector2(random.Next(-1000, 1000) / 3f, random.Next(-1000, 1000) / 3f);
+                component.LocalScale = new Vector2(random.Next(-1000, 1000) / 3f, random.Next(-1000, 1000) / 3f);
+                component.LocalRotation.Angle = random.Next(0, 10) / 3f;
+
+                using (var clone = component.Clone()) {
+                    Assert.AreNotEqual(component.Id, clone.Id);
+                    Assert.AreEqual(component.LocalPosition, clone.LocalPosition);
+                    Assert.AreEqual(component.LocalScale, clone.LocalScale);
+                    Assert.AreEqual(component.LocalRotation.Angle, clone.LocalRotation.Angle);
+                }
+            }
+        }
+
+        [Test]
+        [Category("Unit Test")]
+        public static void Component_DrawOrderTest() {
+            using (var component = new TestComponent()) {
+                component.DrawOrder = 0;
+
+                var drawOrderChanged = false;
+                component.DrawOrderChanged += (object sender, EventArgs e) => drawOrderChanged = true;
+
+                component.DrawOrder = 1;
+                Assert.IsTrue(drawOrderChanged);
+
+                drawOrderChanged = false;
+                component.DrawOrder = 1;
+                Assert.IsFalse(drawOrderChanged);
+            }
+        }
+
+        [Test]
+        [Category("Unit Test")]
+        public static void Component_EnabledTest() {
+            using (var parent = new TestComponent())
+            using (var component = parent.AddChild<TestComponent>()) {
+                parent.Initialize(Substitute.For<IScene>());
+                Assert.IsTrue(component.IsEnabled);
+
+                var enabledChanged = false;
+                component.IsEnabledChanged += (object sender, EventArgs e) => enabledChanged = true;
+                parent.IsEnabled = false;
+                Assert.IsTrue(enabledChanged);
+                Assert.IsFalse(component.IsEnabled);
+
+                enabledChanged = false;
+                parent.IsEnabled = true;
+                Assert.IsTrue(enabledChanged);
+                Assert.IsTrue(component.IsEnabled);
+
+                enabledChanged = false;
+                component.IsEnabled = false;
+                Assert.IsTrue(enabledChanged);
+                Assert.IsFalse(component.IsEnabled);
+
+                enabledChanged = false;
+                parent.IsEnabled = false;
+                Assert.IsFalse(enabledChanged);
+                Assert.IsFalse(component.IsEnabled);
+            }
+        }
 
         [Test]
         [Category("Unit Test")]
@@ -211,6 +330,45 @@
 
         [Test]
         [Category("Unit Test")]
+        public static void Component_ResolveChildren_HasChildrenTest() {
+            using (var component = new WithChildrenComponent()) {
+                component.AddChild<EmptyComponent>();
+                component.AddChild<TestComponent>();
+
+                component.Initialize(Substitute.For<IScene>());
+                Assert.True(component.HasTestComponent);
+                Assert.IsNotNull(component.EmptyComponent);
+            }
+        }
+
+        [Test]
+        [Category("Unit Test")]
+        public static void Component_ResolveChildren_LateAddTest() {
+            using (var component = new WithChildrenComponent()) {
+                component.Initialize(Substitute.For<IScene>());
+                Assert.False(component.HasTestComponent);
+                Assert.IsNull(component.EmptyComponent);
+
+                component.AddChild<TestComponent>();
+                Assert.True(component.HasTestComponent);
+
+                component.AddChild<EmptyComponent>();
+                Assert.IsNotNull(component.EmptyComponent);
+            }
+        }
+
+        [Test]
+        [Category("Unit Test")]
+        public static void Component_ResolveChildren_NoChildrenTest() {
+            using (var component = new WithChildrenComponent()) {
+                component.Initialize(Substitute.For<IScene>());
+                Assert.False(component.HasTestComponent);
+                Assert.IsNull(component.EmptyComponent);
+            }
+        }
+
+        [Test]
+        [Category("Unit Test")]
         public static void Component_SetWorldPositionTest() {
             using (var component1 = new TestComponent("1"))
             using (var component2 = new TestComponent("2")) {
@@ -222,6 +380,77 @@
 
                 component1.SetWorldPosition(Vector2.Zero);
                 Assert.AreEqual(Vector2.Zero, component1.WorldTransform.Position);
+            }
+        }
+
+        [Test]
+        [Category("Unit Test")]
+        public static void Component_UpdateOrderTest() {
+            using (var component = new TestComponent()) {
+                component.UpdateOrder = 0;
+
+                var updateOrderChanged = false;
+                component.UpdateOrderChanged += (object sender, EventArgs e) => updateOrderChanged = true;
+
+                component.UpdateOrder = 1;
+                Assert.IsTrue(updateOrderChanged);
+
+                updateOrderChanged = false;
+                component.UpdateOrder = 1;
+                Assert.IsFalse(updateOrderChanged);
+            }
+        }
+
+        [Test]
+        [Category("Unit Test")]
+        public static void Component_VisibleTest() {
+            using (var parent = new TestComponent())
+            using (var component = parent.AddChild<TestComponent>()) {
+                Assert.IsTrue(component.IsVisible);
+
+                var visibleChanged = false;
+                component.IsVisibleChanged += (object sender, EventArgs e) => visibleChanged = true;
+                component.IsVisible = false;
+                Assert.IsTrue(visibleChanged);
+                Assert.IsFalse(component.IsVisible);
+
+                visibleChanged = false;
+                component.IsEnabled = false;
+                Assert.IsFalse(visibleChanged);
+                Assert.IsFalse(component.IsVisible);
+
+                visibleChanged = false;
+                component.IsEnabled = true;
+                Assert.IsFalse(visibleChanged);
+                Assert.IsFalse(component.IsVisible);
+
+                visibleChanged = false;
+                component.IsVisible = true;
+                Assert.IsTrue(visibleChanged);
+                Assert.IsTrue(component.IsVisible);
+
+                visibleChanged = false;
+                component.IsEnabled = false;
+                Assert.IsTrue(visibleChanged);
+                Assert.IsFalse(component.IsVisible);
+            }
+        }
+
+        private class WithChildrenComponent : BaseComponent {
+#pragma warning disable IDE0044 // Add readonly modifier
+
+            [Child]
+            private TestComponent _testComponent;
+
+#pragma warning restore IDE0044 // Add readonly modifier
+
+            [Child]
+            public EmptyComponent EmptyComponent { get; set; }
+
+            public bool HasTestComponent { get { return this._testComponent != null; } }
+
+            protected override void Initialize() {
+                return;
             }
         }
     }
