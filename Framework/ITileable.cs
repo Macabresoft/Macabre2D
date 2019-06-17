240315cf66c101881faac2f7940d1fb9552336f2
Brett Story <brettmstory@gmail.com>
2019-06-16 19:45:49 -0500

    Refactor: BinaryTileMapComponent implements an extended ITileable interface.

---
+++ Framework/ITileable.cs
@@ -0,0 +1,62 @@
+﻿namespace Macabre2D.Framework {
+
+    using Microsoft.Xna.Framework;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// An interface for tileable components.
+    /// </summary>
+    /// <seealso cref="Macabre2D.Framework.IBoundable"/>
+    public interface ITileable : IBoundable {
+
+        /// <summary>
+        /// Gets or sets the size of the grid. This is how many grid tiles it will contain.
+        /// </summary>
+        /// <value>The size of the map.</value>
+        Point GridSize { get; }
+
+        /// <summary>
+        /// Gets the size of the tiles.
+        /// </summary>
+        /// <value>The size of the tiles.</value>
+        Point TileSize { get; }
+
+        /// <summary>
+        /// Adds the default tile at the specified position.
+        /// </summary>
+        /// <param name="position">The position.</param>
+        void AddTile(Point position);
+
+        /// <summary>
+        /// Removes the tile.
+        /// </summary>
+        /// <param name="position">The position.</param>
+        void RemoveTile(Point position);
+    }
+
+    /// <summary>
+    /// An interface for tileable components of a specific type.
+    /// </summary>
+    /// <typeparam name="T">The type of the tile.</typeparam>
+    public interface ITileable<T> : ITileable {
+
+        /// <summary>
+        /// Gets the available tiles.
+        /// </summary>
+        /// <value>The available tiles.</value>
+        IEnumerable<T> AvailableTiles { get; }
+
+        /// <summary>
+        /// Gets the default tile.
+        /// </summary>
+        /// <value>The default tile.</value>
+        T DefaultTile { get; }
+
+        /// <summary>
+        /// Sets the default tile.
+        /// </summary>
+        /// <param name="newDefault">The new default.</param>
+        /// <returns>A value indicating whether or not the default tile was changed.</returns>
+        bool SetDefaultTile(T newDefault);
+    }
+}
--- Framework/Rendering/BinaryTileMapComponent.cs
+++ Framework/Rendering/BinaryTileMapComponent.cs
@@ -10,18 +10,24 @@
     /// <summary>
     /// A tile map component that is either "on" or "off". The on tiles will show the selected sprite.
     /// </summary>
-    public sealed class BinaryTileMapComponent : BaseComponent, IDrawableComponent, IAssetComponent<Sprite>, ITileMap {
+    public sealed class BinaryTileMapComponent : BaseComponent, IDrawableComponent, IAssetComponent<Sprite>, ITileable<Sprite> {
         private readonly HashSet<Point> _activeTiles = new HashSet<Point>();
         private readonly ResettableLazy<BoundingArea> _boundingArea;
         private Point _mapSize;
         private Sprite _sprite;
-        private Point _tileSize;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="BinaryTileMapComponent"/> class.
         /// </summary>
         public BinaryTileMapComponent() {
             this._boundingArea = new ResettableLazy<BoundingArea>(this.CreateBoundingArea);
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<Sprite> AvailableTiles {
+            get {
+                return new[] { this.Sprite };
+            }
         }
 
         /// <inheritdoc/>
@@ -39,7 +45,14 @@
         public Color Color { get; set; } = Color.White;
 
         /// <inheritdoc/>
-        public Point MapSize {
+        public Sprite DefaultTile {
+            get {
+                return this.Sprite;
+            }
+        }
+
+        /// <inheritdoc/>
+        public Point GridSize {
             get {
                 return this._mapSize;
             }
@@ -82,13 +95,10 @@
             }
         }
 
-        /// <summary>
-        /// Adds an active tile at the specified position.
-        /// </summary>
-        /// <param name="tilePosition">The tile position.</param>
-        public void AddActiveTile(Point tilePosition) {
-            if (this.IsValidTilePosition(tilePosition)) {
-                this._activeTiles.Add(tilePosition);
+        /// <inheritdoc/>
+        public void AddTile(Point position) {
+            if (this.IsValidTilePosition(position)) {
+                this._activeTiles.Add(position);
             }
         }
 
@@ -139,14 +149,6 @@
             }
         }
 
-        /// <summary>
-        /// Removes the active tile.
-        /// </summary>
-        /// <param name="tilePosition">The tile position.</param>
-        public void RemoveActiveTile(Point tilePosition) {
-            this._activeTiles.Remove(tilePosition);
-        }
-
         /// <inheritdoc/>
         public bool RemoveAsset(Guid id) {
             var result = this.HasAsset(id);
@@ -155,6 +157,16 @@
             }
 
             return result;
+        }
+
+        /// <inheritdoc/>
+        public void RemoveTile(Point position) {
+            this._activeTiles.Remove(position);
+        }
+
+        /// <inheritdoc/>
+        public bool SetDefaultTile(Sprite newDefault) {
+            return false;
         }
 
         /// <inheritdoc/>
@@ -173,8 +185,8 @@
             BoundingArea result;
             if (this.Sprite != null) {
                 var inversePixelDensity = GameSettings.Instance.InversePixelsPerUnit;
-                var width = this.MapSize.X * this.Sprite.Size.X * inversePixelDensity;
-                var height = this.MapSize.X * this.Sprite.Size.X * inversePixelDensity;
+                var width = this.GridSize.X * this.Sprite.Size.X * inversePixelDensity;
+                var height = this.GridSize.X * this.Sprite.Size.X * inversePixelDensity;
 
                 var points = new List<Vector2> {
                 this.WorldTransform.Position,
@@ -198,11 +210,11 @@
         }
 
         private bool IsValidTilePosition(Point position) {
-            return position.X >= 0 && position.Y >= 0 && position.X < this.MapSize.X && position.Y < this.MapSize.Y;
+            return position.X >= 0 && position.Y >= 0 && position.X < this.GridSize.X && position.Y < this.GridSize.Y;
         }
 
         private void ResizeActiveTiles() {
-            this._activeTiles.RemoveWhere(x => x.X > this.MapSize.X || x.Y > this.MapSize.Y);
+            this._activeTiles.RemoveWhere(x => x.X > this.GridSize.X || x.Y > this.GridSize.Y);
         }
 
         private void Self_TransformChanged(object sender, EventArgs e) {
