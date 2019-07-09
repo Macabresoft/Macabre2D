7165f612f64760c897338904ff63764eb4198ba4
Brett Story <brettmstory@gmail.com>
2019-07-09 17:26:33 -0500

    Fix: rotation gizmo now updates property in editor. Angle no longer goes past 2Pi if you manually type in.

---
+++ UI/Controls/ValueEditors/RotationEditor.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace Macabre2D.UI.Controls.ValueEditors {
+
+    using Macabre2D.Framework;
+    using System.Windows;
+
+    public partial class RotationEditor : NamedValueEditor<Rotation> {
+
+        public RotationEditor() {
+            this.InitializeComponent();
+        }
+
+        public float Angle {
+            get {
+                return this.Value?.Angle ?? 0f;
+            }
+
+            set {
+                this.UpdateProperty(nameof(Rotation.Angle), this.Angle, value);
+            }
+        }
+
+        protected override void OnValueChanged(Rotation newValue, Rotation oldValue, DependencyObject d) {
+            base.OnValueChanged(newValue, oldValue, d);
+            this.RaisePropertyChanged(nameof(this.Angle));
+
+            if (oldValue != null) {
+                oldValue.AngleChanged -= this.Rotation_AngleChanged;
+            }
+
+            if (newValue != null) {
+                newValue.AngleChanged += this.Rotation_AngleChanged;
+            }
+        }
+
+        private void Rotation_AngleChanged(object sender, System.EventArgs e) {
+            this.RaisePropertyChanged(nameof(this.Angle));
+        }
+    }
+}
