98f038f88b6211ca482c9723578540abf78cfc91
Brett Story <brettmstory@gmail.com>
2019-07-17 17:23:58 -0500

    Refactor: adding in the shell of a status service.

---
+++ UI/ServiceInterfaces/IStatusService.cs
@@ -0,0 +1,14 @@
+﻿namespace Macabre2D.UI.ServiceInterfaces {
+
+    public interface IStatusService {
+        string CurrentStatusMessage { get; set; }
+
+        float PrimaryGridSize { get; }
+
+        float SecondaryGridSize { get; }
+
+        float ViewHeight { get; }
+
+        float ViewWidth { get; }
+    }
+}
