72ed0cc105d40b142ecefb841dd4dd83e5b4c978
Brett Story <brettmstory@gmail.com>
2019-07-19 19:07:43 -0500

    Refactor: beginnings of a component command system.

---
+++ UI/Common/ComponentCommand.cs
@@ -0,0 +1,23 @@
+﻿namespace Macabre2D.UI.Common {
+
+    using GalaSoft.MvvmLight.CommandWpf;
+    using Macabre2D.Framework;
+    using System;
+    using System.Windows.Input;
+
+    public sealed class ComponentCommand<T> where T : BaseComponent {
+        private Action<T> _action;
+
+        public ComponentCommand(string name, Action<T> action) : this(name, action, null) {
+        }
+
+        public ComponentCommand(string name, Action<T> action, Func<bool> canExecuteFunc) {
+            this.Name = name;
+            this._action = action;
+            this.Command = new RelayCommand<T>(action, x => canExecuteFunc?.Invoke() ?? true);
+        }
+
+        public ICommand Command { get; }
+        public string Name { get; }
+    }
+}
