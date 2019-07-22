45539c95b055969dd65ed15f782e5b4b7fb9378c
Brett Story <brettmstory@gmail.com>
2019-07-22 17:32:39 -0500

    Refactor: creating IAutoTileSet interface for future use.

---
+++ Framework/Tiles/IAutoTileSet.cs
@@ -0,0 +1,17 @@
+﻿namespace Macabre2D.Framework {
+
+    /// <summary>
+    /// A tile set that can be auto-generated and edited at runtime.
+    /// </summary>
+    public interface IAutoTileSet : IIdentifiable {
+
+        /// <summary>
+        /// Gets the appropriate sprite for the current tile.
+        /// </summary>
+        /// <param name="directionsOfActiveSprites">
+        /// The directions of active sprites relatively to the requesting tile.
+        /// </param>
+        /// <returns>The sprite.</returns>
+        Sprite GetSprite(CardinalDirection directionsOfActiveSprites);
+    }
+}
