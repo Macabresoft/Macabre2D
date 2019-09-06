56aa9a8b7de71c98cca3f80f214cac1e1ce5ad1d
Brett Story <brettmstory@gmail.com>
2019-09-06 17:10:52 -0500

    Refactor: updating version number as it feels like significant progress has been made.

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.1.2.0")]
-[assembly: AssemblyFileVersion("0.1.2.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
