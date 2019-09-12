04945d988454f1d0aaa4f1600f63a2367c3cfa12
Brett Story <brettmstory@gmail.com>
2019-09-11 19:41:43 -0500

    Fix: Shader now properly builds processor commands for the MGCB file.

--- UI/Models/ShaderAsset.cs
+++ UI/Models/ShaderAsset.cs
@@ -2,6 +2,7 @@
 
     using Macabre2D.Framework;
     using MahApps.Metro.IconPacks;
+    using System.IO;
     using System.Text;
 
     public sealed class ShaderAsset : MetadataAsset {
@@ -21,7 +22,12 @@
         public Shader Shader { get; private set; } = new Shader();
 
         public override void BuildProcessorCommands(StringBuilder contentStringBuilder, string projectDirectoryPath) {
-            base.BuildProcessorCommands(contentStringBuilder, projectDirectoryPath);
+            var path = Path.Combine(projectDirectoryPath, this.GetContentPath());
+            contentStringBuilder.AppendLine($"#begin {path}");
+            contentStringBuilder.AppendLine(@"/importer:EffectImporter");
+            contentStringBuilder.AppendLine(@"/processor:EffectProcessor");
+            contentStringBuilder.AppendLine(@"/processorParam:DebugMode = Auto");
+            contentStringBuilder.AppendLine($@"/build:{path}");
         }
 
         public override void Delete() {
