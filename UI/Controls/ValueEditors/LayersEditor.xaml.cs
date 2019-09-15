b9c80d52ea423047f587b6f512c5ea6b510886ac
Brett Story <brettmstory@gmail.com>
2019-09-15 17:21:03 -0500

    Refactor: layer editor now dynamically populates, so if the layer amount is increased or decreased in the future, it will automatically update.

--- UI/Controls/ValueEditors/LayersEditor.xaml.cs
+++ UI/Controls/ValueEditors/LayersEditor.xaml.cs
@@ -1,13 +1,17 @@
 ﻿namespace Macabre2D.UI.Controls.ValueEditors {
 
     using Macabre2D.Framework;
+    using Macabre2D.UI.Converters;
+    using System;
     using System.Linq;
     using System.Windows;
     using System.Windows.Controls;
+    using System.Windows.Data;
 
     public partial class LayersEditor : NamedValueEditor<Layers> {
 
         public LayersEditor() {
+            this.Loaded += this.LayersEditor_Loaded;
             this.InitializeComponent();
         }
 
@@ -18,6 +22,30 @@
             }
 
             base.OnValueChanged(newValue, oldValue, d);
+        }
+
+        private void LayersEditor_Loaded(object sender, RoutedEventArgs e) {
+            var layers = Enum.GetValues(typeof(Layers)).Cast<Layers>().OrderBy(x => (ushort)x).ToList();
+            layers.Remove(Layers.None);
+            layers.Remove(Layers.All);
+
+            foreach (var layer in layers) {
+                var checkBox = new CheckBox();
+                var contentBinding = new Binding(nameof(CheckBox.Content));
+                contentBinding.Source = layer;
+                contentBinding.Path = new PropertyPath(string.Empty);
+                contentBinding.Converter = new LayerToNameConverter();
+                checkBox.SetBinding(CheckBox.ContentProperty, contentBinding);
+                var isCheckedBinding = new Binding(nameof(CheckBox.IsChecked));
+                isCheckedBinding.Source = this;
+                isCheckedBinding.Path = new PropertyPath(nameof(this.Value));
+                isCheckedBinding.Converter = new LayersToBoolConverter();
+                isCheckedBinding.ConverterParameter = layer;
+                checkBox.SetBinding(CheckBox.IsCheckedProperty, isCheckedBinding);
+                this._layersItemsControl.Items.Add(checkBox);
+            }
+
+            this.Loaded -= this.LayersEditor_Loaded;
         }
     }
 }
