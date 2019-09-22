d484c4ad9b54430e62a81cdc8fc5f7f303ca8adc
Brett Story <brettmstory@gmail.com>
2019-09-21 21:16:08 -0500

    Feature: including the license for all nuget packages and a script that will generate them.

---
+++ GenerateLicences.ps1
@@ -0,0 +1,3 @@
+Split-Path -parent $dte.Solution.FileName | cd
+New-Item -ItemType Directory -Force -Path ".\Licenses"
+@( Get-Project -All | ? { $_.ProjectName } | % { Get-Package -ProjectName $_.ProjectName } ) | Sort -Unique Id | % { $pkg = $_ ; Try { (New-Object System.Net.WebClient).DownloadFile($pkg.LicenseUrl, (Join-Path (pwd) 'licenses\') + $pkg.Id + ".html") } Catch [system.exception] { Write-Host "Could not download license for $($pkg.Id)" } }
