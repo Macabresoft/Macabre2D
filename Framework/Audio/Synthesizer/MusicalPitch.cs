7584eae9e3ca30d9377830c567bfd501a83bb935
Brett Story <brettmstory@gmail.com>
2019-10-22 18:21:27 -0500

    In Progress: musical note and pitch math worked out.

---
+++ Framework/Audio/Synthesizer/MusicalPitch.cs
@@ -0,0 +1,15 @@
+﻿namespace Macabre2D.Framework {
+
+    /// <summary>
+    /// Represents a pitch, which modifies a note up or down.
+    /// </summary>
+    public enum MusicalPitch : byte {
+        Lowest = 1,
+        Lower = 2,
+        Low = 3,
+        Middle = 4,
+        High = 5,
+        Higher = 6,
+        Highest = 7
+    }
+}
