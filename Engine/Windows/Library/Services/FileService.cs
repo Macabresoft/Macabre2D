dfb9b173af707abaf8c952262df4733f23e1d13e
Brett Story <brettmstory@gmail.com>
2020-03-21 18:03:00 -0500

    Refactor: moving editor projects into subfolder.

--- Engine/Windows/Library/Services/FileService.cs
+++ Engine/Windows/Library/Services/FileService.cs
@@ -1,13 +1,11 @@
 ﻿namespace Macabre2D.Engine.Windows.Services {
-
     using Macabre2D.Engine.Windows.ServiceInterfaces;
     using System.IO;
     using System.Reflection;
 
     public sealed class FileService : IFileService {
-
         public FileService() {
-            this.ProjectDirectoryPath = Path.Combine(Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)), "..", "..", "..", "..", @"Project\");
+            this.ProjectDirectoryPath = Path.Combine(Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)), "..", "..", "..", "..", "..", @"Project\");
         }
 
         public string ProjectDirectoryPath { get; }
