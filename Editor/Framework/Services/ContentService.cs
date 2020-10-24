4d6857a8f84bb2b5c07ba492be470be271d79f62
Brett Story <brettmstory@gmail.com>
2020-10-23 20:33:48 -0500

    Adding a ContentService test that will prove the planned way of building content successful.

--- Editor/Framework/Services/ContentService.cs
+++ Editor/Framework/Services/ContentService.cs
@@ -1,10 +1,44 @@
-﻿namespace Macabresoft.Macabre2D.Editor.Framework.Services {
+﻿using System.Diagnostics;
 
+namespace Macabresoft.Macabre2D.Editor.Framework.Services {
+
+    /// <summary>
+    /// Interface for a service that handles MonoGame content for the editor.
+    /// </summary>
     public interface IContentService {
 
-        void Build(bool isDebug);
+        /// <summary>
+        /// Builds the content.
+        /// </summary>
+        /// <param name="isDebug">if set to <c>true</c>, use debug.</param>
+        /// <returns>The exit code of the MGCB process.</returns>
+        int Build(bool isDebug);
     }
 
-    public sealed class ContentService {
+    /// <summary>
+    /// A service that handles MonoGame content for the editor.
+    /// </summary>
+    public sealed class ContentService : IContentService {
+
+        /// <inheritdoc />
+        public int Build(bool isDebug) {
+            var exitCode = -1;
+
+            // TODO: replace this with a real call to mgcb and not just a help call
+            var startInfo = new ProcessStartInfo() {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                FileName = "mgcb",
+                WindowStyle = ProcessWindowStyle.Hidden,
+                Arguments = "--help"
+            };
+
+            using (var process = Process.Start(startInfo)) {
+                process.WaitForExit();
+                exitCode = process.ExitCode;
+            }
+
+            return exitCode;
+        }
     }
 }
---
+++ Tests/Editor/Framework/Services/ContentServiceTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Macabresoft.Macabre2D.Tests.Editor.Framework.Services {
+
+    using FluentAssertions;
+    using FluentAssertions.Execution;
+    using Macabresoft.Macabre2D.Editor.Framework.Services;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public sealed class ContentServiceTests {
+
+        [Test]
+        [Category("Integration Test")]
+        public void Build_ShouldRunMGCB() {
+            var service = new ContentService();
+
+            using (new AssertionScope()) {
+                service.Build(false).Should().Be(0);
+            }
+        }
+    }
+}
