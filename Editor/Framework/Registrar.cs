134c692a00c34b02b0e31c33941f49e11006188d
Brett Story <brettmstory@gmail.com>
2020-10-24 19:33:42 -0500

    Added a UnityContainer and a Registrar.

---
+++ Editor/Framework/Registrar.cs
@@ -0,0 +1,21 @@
+﻿namespace Macabresoft.Macabre2D.Editor.Framework {
+
+    using Macabresoft.Macabre2D.Editor.Framework.Services;
+    using Unity;
+    using Unity.Lifetime;
+
+    /// <summary>
+    /// Registers services and instances to a <see cref="IUnityContainer" />.
+    /// </summary>
+    public static class Registrar {
+
+        /// <summary>
+        /// Registers the services.
+        /// </summary>
+        /// <param name="container">The container.</param>
+        public static void RegisterServices(this IUnityContainer container) {
+            container.RegisterType<IContentService, ContentService>(new SingletonLifetimeManager());
+            container.RegisterType<ISceneService, SceneService>(new SingletonLifetimeManager());
+        }
+    }
+}
--- Editor/UI/App.axaml.cs
+++ Editor/UI/App.axaml.cs
@@ -3,12 +3,17 @@
     using Avalonia;
     using Avalonia.Controls.ApplicationLifetimes;
     using Avalonia.Markup.Xaml;
+    using Macabresoft.Macabre2D.Editor.Framework;
     using Macabresoft.Macabre2D.Editor.Framework.ViewModels;
     using Macabresoft.Macabre2D.Editor.UI.Views;
+    using Unity;
 
     public class App : Application {
+        public IUnityContainer Container { get; private set; }
 
         public override void Initialize() {
+            this.Container = new UnityContainer();
+            this.Container.RegisterServices();
             AvaloniaXamlLoader.Load(this);
         }
 
