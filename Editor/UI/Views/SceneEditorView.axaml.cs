62d5e02962b6019ac7880b452f6bfe809651f231
Brett Story <brettmstory@gmail.com>
2020-10-31 19:02:13 -0500

    Beginnings of SceneEditorView

---
+++ Editor/UI/Views/SceneEditorView.axaml.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Macabresoft.Macabresoft.Macabre2D.Editor.UI.Views {
+    public class SceneEditorView : UserControl {
+        public SceneEditorView() {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent() {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
