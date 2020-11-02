fd0eb715873eb2e1b72060c02bc31671e575513b
Brett Story <brettmstory@gmail.com>
2020-11-01 19:53:44 -0600

    Beginning of CameraControlComponent for the editor.

---
+++ Editor/Framework/MonoGame/Components/CameraControlComponent.cs
@@ -0,0 +1,21 @@
+﻿namespace Macabresoft.Macabre2D.Editor.Library.MonoGame.Components {
+
+    using Macabresoft.Macabre2D.Framework;
+    using System;
+
+    public sealed class CameraControlComponent : GameUpdateableComponent {
+        private CameraComponent _camera;
+
+        public override void Initialize(IGameEntity entity) {
+            base.Initialize(entity);
+
+            if (!this.Entity.TryGetComponent(out this._camera)) {
+                throw new ArgumentNullException(nameof(this._camera));
+            }
+        }
+
+        public override void Update(FrameTime frameTime, InputState inputState) {
+            throw new NotImplementedException();
+        }
+    }
+}
