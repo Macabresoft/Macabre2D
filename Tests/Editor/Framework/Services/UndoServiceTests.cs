e5f1f85f9d7df929f46d320cc7da9766c151f5bd
Brett Story <brettmstory@gmail.com>
2020-11-21 19:10:00 -0600

    More UndoServiceTests.

--- Tests/Editor/Framework/Services/UndoServiceTests.cs
+++ Tests/Editor/Framework/Services/UndoServiceTests.cs
@@ -2,14 +2,14 @@
     using System;
     using System.Collections.Generic;
     using FluentAssertions;
-    using Macabresoft.Macabre2D.Editor.Library.Services;
-    using Macabresoft.Macabre2D.Framework;
+    using Macabre2D.Editor.Library.Services;
+    using Macabre2D.Framework;
     using NUnit.Framework;
 
     [TestFixture]
     public sealed class UndoServiceTests {
         private const int MultiGenerationCount = 100;
-        
+
         [Test]
         [Category("Unit Tests")]
         public void Do_Should_PerformChanges() {
@@ -66,7 +66,7 @@
 
             undoService.CanUndo.Should().BeTrue();
         }
-        
+
         [Test]
         [Category("Unit Tests")]
         public void Undo_Should_SetCanUndoToTrue() {
@@ -83,21 +83,71 @@
 
         [Test]
         [Category("Unit Tests")]
+        public void Do_Should_TriggerPropertyChanged() {
+            var undoService = new UndoService();
+            var testObject = new TestObject();
+            var hasPropertyChanged = false;
+
+            undoService.Do(
+                () => testObject.TestProperty = TestObject.NewValue,
+                () => testObject.TestProperty = TestObject.OldValue,
+                () => hasPropertyChanged = true);
+
+            hasPropertyChanged.Should().BeTrue();
+        }
+
+        [Test]
+        [Category("Unit Tests")]
+        public void Undo_Should_TriggerPropertyChanged() {
+            var undoService = new UndoService();
+            var testObject = new TestObject();
+            var hasPropertyChanged = false;
+
+            undoService.Do(
+                () => testObject.TestProperty = TestObject.NewValue,
+                () => testObject.TestProperty = TestObject.OldValue,
+                () => hasPropertyChanged = true);
+
+            hasPropertyChanged = false;
+
+            undoService.Undo();
+            hasPropertyChanged.Should().BeTrue();
+        }
+
+        [Test]
+        [Category("Unit Tests")]
+        public void Redo_Should_TriggerPropertyChanged() {
+            var undoService = new UndoService();
+            var testObject = new TestObject();
+            var hasPropertyChanged = false;
+
+            undoService.Do(
+                () => testObject.TestProperty = TestObject.NewValue,
+                () => testObject.TestProperty = TestObject.OldValue,
+                () => hasPropertyChanged = true);
+
+            undoService.Undo();
+            hasPropertyChanged = false;
+            undoService.Redo();
+
+            hasPropertyChanged.Should().BeTrue();
+        }
+
+        [Test]
+        [Category("Unit Tests")]
         public void MultiGenerationTest() {
             var undoService = new UndoService();
-
             var testObjects = new List<TestObject>();
-            
-            for (var i = 0; i < MultiGenerationCount; i++)
-            {
+
+            for (var i = 0; i < MultiGenerationCount; i++) {
                 var testObject = new TestObject();
                 undoService.Do(
                     () => testObject.TestProperty = TestObject.NewValue,
                     () => testObject.TestProperty = TestObject.OldValue);
-                
+
                 testObjects.Add(testObject);
             }
-            
+
             testObjects.Reverse();
 
             testObjects.Count.Should().Be(MultiGenerationCount);
@@ -106,7 +156,7 @@
                 undoService.Undo();
                 testObject.TestProperty.Should().Be(TestObject.OldValue);
             }
-            
+
             testObjects.Reverse();
 
             testObjects.Count.Should().Be(MultiGenerationCount);
@@ -116,7 +166,7 @@
                 testObject.TestProperty.Should().Be(TestObject.NewValue);
             }
         }
-        
+
         private sealed class TestObject : NotifyPropertyChanged {
             public TestObject() {
                 this.TestProperty = OldValue;
