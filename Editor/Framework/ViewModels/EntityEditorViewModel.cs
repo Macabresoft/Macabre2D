a9e35f25b475e740d556f27397cd27b9e249ed7d
Brett Story <brettmstory@gmail.com>
2020-12-05 14:26:26 -0600

    Adding an entity editor view.

---
+++ Editor/Framework/ViewModels/EntityEditorViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Macabresoft.Macabre2D.Editor.Library.ViewModels {
+    using Macabresoft.Macabre2D.Editor.Library.Services;
+    using Unity;
+
+    public class EntityEditorViewModel : ViewModelBase {
+        private readonly ISelectionService _selectionService;
+
+        public EntityEditorViewModel() {
+        }
+
+        [InjectionConstructor]
+        public EntityEditorViewModel(ISelectionService selectionService) {
+            this._selectionService = selectionService;
+        }
+    }
+}
---
+++ Editor/UI/Views/EntityEditorView.axaml.cs
@@ -0,0 +1,19 @@
+﻿namespace Macabresoft.Macabre2D.Editor.UI.Views {
+    using System.Linq;
+    using Avalonia;
+    using Avalonia.Controls;
+    using Avalonia.Markup.Xaml;
+    using Avalonia.VisualTree;
+    using Macabresoft.Macabre2D.Editor.Library.ViewModels;
+
+    public class EntityEditorView : UserControl {
+        public EntityEditorView() {
+            this.DataContext = Resolver.Resolve<EntityEditorViewModel>();
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent() {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
