09302957aa9d5c68faa6401d3f1aeb2c82030048
Brett Story <brettmstory@gmail.com>
2020-12-10 17:28:38 -0600

    Base of ValueEditor.

---
+++ Editor/Library/Models/IValueEditor.cs
@@ -0,0 +1,9 @@
+﻿namespace Macabresoft.Macabre2D.Editor.Library.Models {
+    using System.ComponentModel;
+
+    public interface INamedValueEditor : INotifyPropertyChanged {
+        object Owner { get; }
+        
+        object
+    }
+}
