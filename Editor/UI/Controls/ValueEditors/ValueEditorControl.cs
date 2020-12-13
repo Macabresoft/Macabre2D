420b6d038a7eba3953683892cfc47929d5a66bc3
Brett Story <brettmstory@gmail.com>
2020-12-13 07:10:06 -0600

    Value Editors can now perform LostFocus.

--- Editor/UI/Controls/ValueEditors/ValueEditorControl.cs
+++ Editor/UI/Controls/ValueEditors/ValueEditorControl.cs
@@ -2,7 +2,7 @@
     using System;
     using Avalonia;
     using Avalonia.Controls;
-    using Avalonia.Markup.Xaml;
+    using Avalonia.Interactivity;
     using Macabresoft.Core;
     using Macabresoft.Macabre2D.Editor.Library.Models;
 
@@ -13,13 +13,35 @@
         public static readonly StyledProperty<T> ValueProperty =
             AvaloniaProperty.Register<ValueEditorControl<T>, T>(nameof(Value), notifying: OnValueChanging);
 
+        public static readonly DirectProperty<ValueEditorControl<T>, T> IntermediaryValueProperty =
+            AvaloniaProperty.RegisterDirect<ValueEditorControl<T>, T>(
+                nameof(IntermediaryValue),
+                editor => editor.IntermediaryValue,
+                (editor, value) => editor.IntermediaryValue = value);
+
+
         public static readonly StyledProperty<string> TitleProperty =
             AvaloniaProperty.Register<ValueEditorControl<T>, string>(nameof(Title));
 
+        public static readonly StyledProperty<bool> UpdateOnLostFocusProperty =
+            AvaloniaProperty.Register<ValueEditorControl<T>, bool>(nameof(UpdateOnLostFocus), true);
 
         public static readonly StyledProperty<string> ValuePropertyNameProperty =
             AvaloniaProperty.Register<ValueEditorControl<T>, string>(nameof(ValuePropertyName));
-        
+
+        private T _intermediaryValue;
+
+        public T IntermediaryValue {
+            get => this._intermediaryValue;
+            set {
+                if (!this.UpdateOnLostFocus) {
+                    this.SetValue(this.Value, value);
+                }
+
+                this.SetAndRaise(IntermediaryValueProperty, ref this._intermediaryValue, value);
+            }
+        }
+
         public object Owner {
             get => this.GetValue(OwnerProperty);
             set => this.SetValue(OwnerProperty, value);
@@ -28,6 +50,11 @@
         public string Title {
             get => this.GetValue(TitleProperty);
             set => this.SetValue(TitleProperty, value);
+        }
+
+        public bool UpdateOnLostFocus {
+            get => this.GetValue(UpdateOnLostFocusProperty);
+            set => this.SetValue(UpdateOnLostFocusProperty, value);
         }
 
         public T Value {
@@ -40,19 +67,28 @@
             set => this.SetValue(ValuePropertyNameProperty, value);
         }
 
-        private void OnValueChanged(T originalValue, T updatedValue) {
-            this.ValueChanged.SafeInvoke(this, new ValueChangedEventArgs<T>(originalValue, updatedValue));
+        private bool HasValueChanged() {
+            if (this.Value != null) {
+                return !this.Value.Equals(this.IntermediaryValue);
+            }
+
+            return this.IntermediaryValue != null;
         }
 
         private static void OnValueChanging(IAvaloniaObject control, bool isBeforeChange) {
-            if (!isBeforeChange && control is ValueEditorControl<T> valueEditor &&
-                valueEditor.Owner != null &&
-                !string.IsNullOrWhiteSpace(valueEditor.ValuePropertyName)) {
-                var originalValue = valueEditor.Owner.GetPropertyValue(valueEditor.ValuePropertyName);
+            if (!isBeforeChange && control is ValueEditorControl<T> valueEditor && valueEditor.HasValueChanged()) {
+                valueEditor.SetAndRaise(IntermediaryValueProperty, ref valueEditor._intermediaryValue, valueEditor.Value);
+            }
+        }
 
-                if (originalValue is T value) {
-                    valueEditor.OnValueChanged(value, valueEditor.Value);
-                }
+        private void SetValue(T originalValue, T updatedValue) {
+            this.Value = updatedValue;
+            this.ValueChanged.SafeInvoke(this, new ValueChangedEventArgs<T>(originalValue, updatedValue));
+        }
+
+        protected void ValueEditor_OnLostFocus(object sender, RoutedEventArgs e) {
+            if (this.UpdateOnLostFocus && this.HasValueChanged()) {
+                this.SetValue(this.Value, this.IntermediaryValue);
             }
         }
 
