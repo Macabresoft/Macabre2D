58d40eb2ea57f375a78eab3f92e5201c6a008b5c
Brett Story <brettmstory@gmail.com>
2020-12-17 17:38:04 -0600

    Bool editor with IsEnabled added to the entity editor.

---
+++ Editor/UI/Controls/ValueEditors/BoolEditor.axaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Macabresoft.Macabre2D.Editor.UI.Controls.ValueEditors {
+    using Avalonia.Markup.Xaml;
+
+    public class BoolEditor : ValueEditorControl<bool> {
+        public BoolEditor() {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent() {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
