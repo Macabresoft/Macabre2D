c0aaae2cda47465d9ad4f33d3ced88061bb6b4d4
Brett Story <brettmstory@gmail.com>
2020-12-27 15:13:25 -0600

    Beginnings of ColliderEditor.

---
+++ Editor/UI/Controls/ValueEditors/ColliderEditor.axaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Macabresoft.Macabre2D.Editor.UI.Controls.ValueEditors {
+    using Avalonia.Markup.Xaml;
+    using Macabresoft.Macabre2D.Framework;
+
+    public class ColliderEditor : ValueEditorControl<Collider> {
+        public ColliderEditor() {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent() {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
