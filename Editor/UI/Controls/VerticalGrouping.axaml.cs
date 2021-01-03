92a9318f94bc4bf668e4adff3bea4f1dc2597ad6
Brett Story <brettmstory@gmail.com>
2021-01-03 09:01:41 -0600

    Adding VerticalGrouping control for groupings that don't need the same level of separation as a groupbox.

---
+++ Editor/UI/Controls/VerticalGrouping.axaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Macabresoft.Macabre2D.Editor.UI.Controls {
+    using Avalonia.Controls.Primitives;
+    using Avalonia.Markup.Xaml;
+
+    public class VerticalGrouping : HeaderedContentControl {
+        public VerticalGrouping() {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent() {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
