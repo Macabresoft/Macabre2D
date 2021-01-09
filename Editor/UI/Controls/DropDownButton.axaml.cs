a43fda61da1ca8954b50952dce17ed80b98310c8
Brett Story <brettmstory@gmail.com>
2021-01-09 09:43:06 -0600

    Moved psudeocode for DropDownButton to its own file.

---
+++ Editor/UI/Controls/DropDownButton.axaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Macabresoft.Macabre2D.Editor.UI.Controls {
+    using Avalonia.Controls;
+    using Avalonia.Markup.Xaml;
+
+    public class DropDownButton : ContentControl {
+        public DropDownButton() {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent() {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
