c81d06fc15089816057aad3a121cba65a189b7aa
Brett Story <brettmstory@gmail.com>
2021-01-30 14:19:18 -0600

    Upgrading nuget packages.

---
+++ Framework/Hierarchy/Assets/SceneAsset.cs
@@ -0,0 +1,13 @@
+﻿namespace Macabresoft.Macabre2D.Framework {
+    
+    /// <summary>
+    /// An asset which contains a <see cref="IGameScene"/>.
+    /// </summary>
+    public class SceneAsset : BaseContentAsset<GameScene> {
+        
+        /// <summary>
+        /// The file extension for a serialized <see cref="GameScene"/>.
+        /// </summary>
+        public const string FileExtension = ".m2dscene";
+    }
+}
