85315630dce40507f0227abe71f72528ddf6f9a8
Brett Story <brettmstory@gmail.com>
2021-04-01 17:04:16 -0500

    Beginnings of a path service.

---
+++ Editor/Library/Services/PathService.cs
@@ -0,0 +1,34 @@
+﻿namespace Macabresoft.Macabre2D.Editor.Library.Services {
+
+    /// <summary>
+    /// Interface for a service which provides typical paths for the editor to access the project and its content.
+    /// </summary>
+    public interface IPathService {
+        /// <summary>
+        /// Gets the project directory path.
+        /// </summary>
+        string ProjectDirectoryPath { get; }
+        
+        /// <summary>
+        /// Gets the content directory path.
+        /// </summary>
+        string ContentDirectoryPath { get; }
+        
+        /// <summary>
+        /// Gets the metadata directory path.
+        /// </summary>
+        string MetadataDirectoryPath { get; }
+        
+        /// <summary>
+        /// Gets the metadata archive directory path.
+        /// </summary>
+        string MetadataArchiveDirectoryPath { get; }
+    }
+    
+    /// <summary>
+    /// A service which provides typical paths for the editor to access the project and its content.
+    /// </summary>
+    public class PathService {
+
+    }
+}
