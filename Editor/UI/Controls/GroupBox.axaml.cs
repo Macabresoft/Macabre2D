c9d0be94c8ab704458eae9f64e5c380dc0f0682a
Brett Story <brettmstory@gmail.com>
2021-06-14 17:07:36 -0500

    Removing the close button from group boxes.

--- Editor/UI/Controls/GroupBox.axaml.cs
+++ Editor/UI/Controls/GroupBox.axaml.cs
@@ -7,18 +7,12 @@
     using ReactiveUI;
 
     public class GroupBox : HeaderedContentControl {
-        public static readonly StyledProperty<object> CloseCommandParameterProperty =
-            AvaloniaProperty.Register<GroupBox, object>(nameof(CloseCommand));
-
-        public static readonly StyledProperty<ICommand> CloseCommandProperty =
-            AvaloniaProperty.Register<GroupBox, ICommand>(nameof(CloseCommand));
+        private static readonly DirectProperty<GroupBox, bool> HideContentProperty =
+            AvaloniaProperty.RegisterDirect<GroupBox, bool>(nameof(HideContent), x => !x.ShowContent);
 
         private static readonly DirectProperty<GroupBox, bool> ShowContentProperty =
             AvaloniaProperty.RegisterDirect<GroupBox, bool>(nameof(ShowContent), x => x.ShowContent);
 
-        private static readonly DirectProperty<GroupBox, bool> HideContentProperty =
-            AvaloniaProperty.RegisterDirect<GroupBox, bool>(nameof(HideContent), x => !x.ShowContent);
-        
         // ReSharper disable once UnusedMember.Local
         private static readonly DirectProperty<GroupBox, ICommand> ToggleContentCommandProperty =
             AvaloniaProperty.RegisterDirect<GroupBox, ICommand>(nameof(ToggleContentCommand), x => x.ToggleContentCommand);
@@ -30,26 +24,16 @@
             this.InitializeComponent();
         }
 
-        public ICommand ToggleContentCommand { get; }
-
-        public ICommand CloseCommand {
-            get => this.GetValue(CloseCommandProperty);
-            set => this.SetValue(CloseCommandProperty, value);
-        }
-
-        public object CloseCommandParameter {
-            get => this.GetValue(CloseCommandParameterProperty);
-            set => this.SetValue(CloseCommandParameterProperty, value);
-        }
-
         public bool HideContent => !this.ShowContent;
+
+        public ICommand ToggleContentCommand { get; }
 
         public bool ShowContent {
             get => this._showContent;
             private set {
                 this.SetAndRaise(ShowContentProperty, ref this._showContent, value);
-                RaisePropertyChanged(HideContentProperty, Optional<bool>.Empty, !this.ShowContent);
-            } 
+                this.RaisePropertyChanged(HideContentProperty, Optional<bool>.Empty, !this.ShowContent);
+            }
         }
 
         private void CollapseContent() {
