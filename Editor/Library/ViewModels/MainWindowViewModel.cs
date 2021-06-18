ff12190701ddf30e3cc9db8e9b35712b19264e96
Brett Story <brettmstory@gmail.com>
2021-06-18 16:15:48 -0500

    Potential solution for native menus in MacOS.

--- Editor/Library/ViewModels/MainWindowViewModel.cs
+++ Editor/Library/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,10 @@
 ﻿namespace Macabresoft.Macabre2D.Editor.Library.ViewModels {
     using System;
-    using System.Reactive;
     using System.Windows.Input;
+    using Avalonia;
+    using Avalonia.Controls;
+    using Avalonia.Platform;
+    using Macabresoft.Core;
     using Macabresoft.Macabre2D.Editor.Library.Services;
     using ReactiveUI;
     using Unity;
@@ -10,16 +13,13 @@
     /// The view model for the main window.
     /// </summary>
     public class MainWindowViewModel : ViewModelBase {
-        private readonly ReactiveCommand<Unit, Unit> _undoCommand;
-        private readonly ReactiveCommand<Unit, Unit> _redoCommand;
-
         /// <summary>
         /// Initializes a new instance of the <see cref="MainWindowViewModel" /> class.
         /// </summary>
         /// <remarks>This constructor only exists for design time XAML.</remarks>
         public MainWindowViewModel() : base() {
         }
-        
+
         /// <summary>
         /// Initializes a new instance of the <see cref="MainWindowViewModel" /> class.
         /// </summary>
@@ -32,28 +32,53 @@
             }
 
             this.SelectionService = selectionService ?? throw new ArgumentNullException(nameof(selectionService));
-            
-            this._undoCommand = ReactiveCommand.Create(
-                undoService.Undo,
-                undoService.WhenAnyValue(x => x.CanUndo));
-            this._redoCommand = ReactiveCommand.Create(
+
+            this.ExitCommand = ReactiveCommand.Create<Window>(this.Exit);
+            this.RedoCommand = ReactiveCommand.Create(
                 undoService.Redo,
                 undoService.WhenAnyValue(x => x.CanRedo));
+            this.UndoCommand = ReactiveCommand.Create(
+                undoService.Undo,
+                undoService.WhenAnyValue(x => x.CanUndo));
+            this.ViewSourceCommand = ReactiveCommand.Create(this.ViewSource);
         }
-        
+
+        /// <summary>
+        /// Gets the command to exit the application.
+        /// </summary>
+        public ICommand ExitCommand { get; }
+
+        /// <summary>
+        /// Gets the command to redo a previously undone operation.
+        /// </summary>
+        public ICommand RedoCommand { get; }
+
         /// <summary>
         /// Gets the selection service.
         /// </summary>
         public ISelectionService SelectionService { get; }
 
         /// <summary>
-        /// Gets the command to undo the previous operation.
+        /// Gets a value indicating whether or not the non-native menu should be shown. The native menu is for MacOS only.
         /// </summary>
-        public ICommand UndoCommand => this._undoCommand;
+        public bool ShowNonNativeMenu => AvaloniaLocator.Current.GetService<IRuntimePlatform>().GetRuntimeInfo().OperatingSystem != OperatingSystemType.OSX;
 
         /// <summary>
-        /// Gets the command to redo a previously undone operation.
+        /// Gets the command to undo the previous operation.
         /// </summary>
-        public ICommand RedoCommand => this._redoCommand;
+        public ICommand UndoCommand { get; }
+
+        /// <summary>
+        /// Gets the command to view the source code.
+        /// </summary>
+        public ICommand ViewSourceCommand { get; }
+
+        private void Exit(Window window) {
+            window?.Close();
+        }
+
+        private void ViewSource() {
+            WebHelper.OpenInBrowser("https://github.com/Macabresoft/Macabre2D");
+        }
     }
 }
--- Editor/UI/Views/MainWindow.axaml.cs
+++ Editor/UI/Views/MainWindow.axaml.cs
@@ -1,22 +1,12 @@
 ﻿namespace Macabresoft.Macabre2D.Editor.UI.Views {
     using Avalonia.Controls;
-    using Avalonia.Interactivity;
     using Avalonia.Markup.Xaml;
-    using Macabresoft.Core;
     using Macabresoft.Macabre2D.Editor.Library.ViewModels;
 
     public class MainWindow : Window {
         internal void InitializeComponent() {
             this.DataContext = Resolver.Resolve<MainWindowViewModel>();
             AvaloniaXamlLoader.Load(this);
-        }
-
-        private void Exit_Click(object sender, RoutedEventArgs e) {
-            this.Close();
-        }
-
-        private void ViewSource_Click(object sender, RoutedEventArgs e) {
-            WebHelper.OpenInBrowser("https://github.com/Macabresoft/Macabre2D");
         }
     }
 }
