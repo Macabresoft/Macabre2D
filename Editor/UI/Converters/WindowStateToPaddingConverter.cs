78cbe06bf8ec07070945286a06ef40ce96fcd609
Brett Story <brettmstory@gmail.com>
2021-06-19 09:06:39 -0500

    Application named, title bar distinguished, fixed pixel overflow on windows, and lined up a lot of colors to the ~brand~.

---
+++ Editor/UI/Converters/WindowStateToPaddingConverter.cs
@@ -0,0 +1,26 @@
+﻿namespace Macabresoft.Macabre2D.Editor.UI.Converters {
+    using System;
+    using System.Globalization;
+    using Avalonia;
+    using Avalonia.Controls;
+    using Avalonia.Data.Converters;
+    using Avalonia.Platform;
+
+    public class WindowStateToPaddingConverter : IValueConverter {
+        public static readonly Thickness MatchingPadding = new Thickness(8);
+        public static readonly Thickness UnmatchingPadding = new Thickness(0);
+        
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            var operatingSystem = AvaloniaLocator.Current.GetService<IRuntimePlatform>().GetRuntimeInfo().OperatingSystem;
+            if (operatingSystem == OperatingSystemType.WinNT && value is WindowState and (WindowState.Maximized or WindowState.FullScreen)) {
+                return MatchingPadding;
+            }
+
+            return UnmatchingPadding;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
