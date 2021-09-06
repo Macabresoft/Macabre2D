7e5d6068e8a09a3065ef49b2fcfcda9d9d3c3b65
Brett Story <brettmstory@gmail.com>
2021-09-06 10:17:37 -0500

    AutoTileSetEditorDialog now uses BaseDialog.

--- UI/ProjectEditor/Views/Dialogs/AutoTileSetEditorDialog.axaml.cs
+++ UI/ProjectEditor/Views/Dialogs/AutoTileSetEditorDialog.axaml.cs
@@ -1,13 +1,12 @@
 ﻿namespace Macabresoft.Macabre2D.UI.ProjectEditor.Views.Dialogs {
-    using Avalonia.Controls;
     using Avalonia.Markup.Xaml;
     using Macabresoft.Macabre2D.UI.Common.ViewModels.Dialogs;
     using Unity;
 
-    public class AutoTileSetEditorDialog : Window {
+    public class AutoTileSetEditorDialog : BaseDialog {
         public AutoTileSetEditorDialog() {
         }
-        
+
         [InjectionConstructor]
         public AutoTileSetEditorDialog(AutoTileSetEditorViewModel viewModel) {
             this.DataContext = viewModel;
--- UI/ProjectEditor/Views/Dialogs/BaseDialog.axaml.cs
+++ UI/ProjectEditor/Views/Dialogs/BaseDialog.axaml.cs
@@ -5,8 +5,16 @@ namespace Macabresoft.Macabre2D.UI.ProjectEditor.Views.Dialogs {
     using Avalonia.Media;
 
     public class BaseDialog : Window {
-        public static readonly StyledProperty<StreamGeometry> VectorIconProperty = 
+        public static readonly StyledProperty<Menu> MenuProperty =
+            AvaloniaProperty.Register<BaseDialog, Menu>(nameof(Menu), defaultBindingMode: BindingMode.OneWay);
+
+        public static readonly StyledProperty<StreamGeometry> VectorIconProperty =
             AvaloniaProperty.Register<BaseDialog, StreamGeometry>(nameof(VectorIcon), defaultBindingMode: BindingMode.OneWay);
+
+        public Menu Menu {
+            get => this.GetValue(MenuProperty);
+            set => this.SetValue(MenuProperty, value);
+        }
 
         public StreamGeometry VectorIcon {
             get => this.GetValue(VectorIconProperty);
