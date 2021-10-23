eb62a50b4567d69ceb1806d9d51a7106d874a181
Brett Story <brettmstory@gmail.com>
2021-10-23 13:14:40 -0500

    Removing show non native menu stuff

--- UI/Common/Helpers/WindowHelper.cs
+++ UI/Common/Helpers/WindowHelper.cs
@@ -1,18 +1,12 @@
 ﻿namespace Macabresoft.Macabre2D.UI.Common {
-    using System;
     using System.Windows.Input;
-    using Avalonia;
     using Avalonia.Controls;
-    using Avalonia.Platform;
     using ReactiveUI;
 
     /// <summary>
     /// Helper class for windows.
     /// </summary>
     public static class WindowHelper {
-        private static readonly Lazy<bool> LazyShowNonNativeMenu = new(
-            () => AvaloniaLocator.Current.GetService<IRuntimePlatform>().GetRuntimeInfo().OperatingSystem == OperatingSystemType.WinNT);
-
         /// <summary>
         /// Gets a command to close a dialog.
         /// </summary>
@@ -22,11 +16,6 @@
         /// Gets a command to minimize a window.
         /// </summary>
         public static ICommand MinimizeCommand { get; } = ReactiveCommand.Create<IWindow>(Minimize);
-
-        /// <summary>
-        /// Gets a value indicating whether or not the non-native menu should be shown. The native menu is for MacOS only.
-        /// </summary>
-        public static bool ShowNonNativeMenu => LazyShowNonNativeMenu.Value;
 
         /// <summary>
         /// Gets a command to toggle a window's state.
