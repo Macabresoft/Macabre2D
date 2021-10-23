80b54fdf62cb0258caacd09776eca8c0de49edf7
Brett Story <brettmstory@gmail.com>
2021-10-23 14:00:33 -0500

    Dialogs prettied up.

--- UI/Common/Dialogs/BaseDialog.axaml.cs
+++ UI/Common/Dialogs/BaseDialog.axaml.cs
@@ -3,11 +3,16 @@ namespace Macabresoft.Macabre2D.UI.Common {
     using Avalonia;
     using Avalonia.Controls;
     using Avalonia.Data;
+    using Avalonia.Input;
+    using Avalonia.Interactivity;
     using Avalonia.Media;
 
     public class BaseDialog : Window, IWindow {
         public static readonly StyledProperty<ICommand> CloseCommandProperty =
             AvaloniaProperty.Register<BaseDialog, ICommand>(nameof(CloseCommand), defaultBindingMode: BindingMode.OneWay, defaultValue: WindowHelper.CloseDialogCommand);
+
+        public static readonly StyledProperty<object> LeftOfMenuTitleBarContentProperty =
+            AvaloniaProperty.Register<BaseDialog, object>(nameof(LeftOfMenuTitleBarContent), defaultBindingMode: BindingMode.OneWay);
 
         public static readonly StyledProperty<Menu> MenuProperty =
             AvaloniaProperty.Register<BaseDialog, Menu>(nameof(Menu), defaultBindingMode: BindingMode.OneWay);
@@ -23,6 +28,11 @@ namespace Macabresoft.Macabre2D.UI.Common {
             set => this.SetValue(CloseCommandProperty, value);
         }
 
+        public object LeftOfMenuTitleBarContent {
+            get => this.GetValue(LeftOfMenuTitleBarContentProperty);
+            set => this.SetValue(LeftOfMenuTitleBarContentProperty, value);
+        }
+
         public Menu Menu {
             get => this.GetValue(MenuProperty);
             set => this.SetValue(MenuProperty, value);
@@ -36,6 +46,16 @@ namespace Macabresoft.Macabre2D.UI.Common {
         public StreamGeometry VectorIcon {
             get => this.GetValue(VectorIconProperty);
             set => this.SetValue(VectorIconProperty, value);
+        }
+
+        private void TitleBar_OnDoubleTapped(object sender, RoutedEventArgs e) {
+            if (WindowHelper.ToggleWindowStateCommand.CanExecute(this)) {
+                WindowHelper.ToggleWindowStateCommand.Execute(this);
+            }
+        }
+
+        private void TitleBar_OnPointerPressed(object sender, PointerPressedEventArgs e) {
+            this.BeginMoveDrag(e);
         }
     }
 }
