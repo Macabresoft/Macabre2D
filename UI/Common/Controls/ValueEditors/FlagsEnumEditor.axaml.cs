98ce67c35026e97327647c05cbeabcb1bdd56fd7
Brett Story <brettmstory@gmail.com>
2022-02-09 17:08:47 -0600

    Can select all or clear selection for flags enums

--- UI/Common/Controls/ValueEditors/FlagsEnumEditor.axaml.cs
+++ UI/Common/Controls/ValueEditors/FlagsEnumEditor.axaml.cs
@@ -1,6 +1,7 @@
 namespace Macabresoft.Macabre2D.UI.Common;
 
 using System;
+using System.Linq;
 using System.Windows.Input;
 using Avalonia;
 using Avalonia.Controls;
@@ -14,9 +15,14 @@ public class FlagsEnumEditor : ValueEditorControl<object> {
             nameof(EnumType),
             editor => editor.EnumType);
 
+    public static readonly DirectProperty<FlagsEnumEditor, ICommand> SelectAllCommandProperty =
+        AvaloniaProperty.RegisterDirect<FlagsEnumEditor, ICommand>(
+            nameof(SelectAllCommand),
+            editor => editor.SelectAllCommand);
+
     public static readonly DirectProperty<FlagsEnumEditor, ICommand> ToggleValueCommandProperty =
         AvaloniaProperty.RegisterDirect<FlagsEnumEditor, ICommand>(
-            nameof(EnumType),
+            nameof(ToggleValueCommand),
             editor => editor.ToggleValueCommand);
 
     public FlagsEnumEditor() : this(null) {
@@ -25,16 +31,35 @@ public class FlagsEnumEditor : ValueEditorControl<object> {
     [InjectionConstructor]
     public FlagsEnumEditor(ValueControlDependencies dependencies) : base(dependencies) {
         this.EnumType = dependencies?.ValueType;
+        this.ClearCommand = ReactiveCommand.Create(this.Clear);
+        this.SelectAllCommand = ReactiveCommand.Create(this.SelectAll);
         this.ToggleValueCommand = ReactiveCommand.Create<object>(this.ToggleValue);
         this.InitializeComponent();
     }
 
+    public ICommand ClearCommand { get; }
+
     public Type EnumType { get; }
+
+    public ICommand SelectAllCommand { get; }
 
     public ICommand ToggleValueCommand { get; }
 
+    private void Clear() {
+        this.Value = Enum.Parse(this.EnumType, 0.ToString());
+    }
+
     private void InitializeComponent() {
         AvaloniaXamlLoader.Load(this);
+    }
+
+    private void SelectAll() {
+        var enums = Enum.GetValues(this.EnumType).Cast<object>().Select(Convert.ToInt32);
+        var all = enums.Aggregate(0, (current, value) => current | value);
+        var original = Convert.ToInt32(this.Value);
+        if (all != original) {
+            this.Value = Enum.Parse(this.EnumType, all.ToString());
+        }
     }
 
     private void SelectingItemsControl_OnSelectionChanged(object sender, SelectionChangedEventArgs e) {
