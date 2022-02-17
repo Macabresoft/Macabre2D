aac105b23c55c65d4b360fd770b43de8e34b1399
Brett Story <brettmstory@gmail.com>
2022-02-17 16:49:51 -0600

    Slightly better drag drop insert logic

--- UI/Editor/Views/SceneTreeView.axaml.cs
+++ UI/Editor/Views/SceneTreeView.axaml.cs
@@ -1,12 +1,11 @@
 namespace Macabresoft.Macabre2D.UI.Editor;
 
 using System;
-using System.Linq;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
-using Avalonia.VisualTree;
 using Macabresoft.AvaloniaEx;
 using Macabresoft.Macabre2D.Framework;
 using Macabresoft.Macabre2D.UI.Common;
@@ -17,9 +16,12 @@ public class SceneTreeView : UserControl {
             nameof(ViewModel),
             editor => editor.ViewModel);
 
-    private TreeViewItem _currentDropTarget;
+    private static readonly Thickness BottomHighlightThickness = new(0D, 0D, 0D, 2D);
+    private static readonly Thickness EmptyHighlightThickness = new(0D);
+    private static readonly Thickness TopHighlightThickness = new(0D, 2D, 0D, 0D);
 
-    private Guid _dragTarget;
+    private TreeViewItem _currentDropTarget;
+    private Guid _draggedEntityId;
     private bool _isDragging;
 
     public SceneTreeView() {
@@ -33,11 +35,9 @@ public class SceneTreeView : UserControl {
 
     private void Drag(object sender, DragEventArgs e) {
         if (e.Source is IControl { DataContext: { } } control) {
-            this.ResetDropTarget(control);
-            this.SetDropHighlight(e);
+            this.ResetDropTarget(control, e);
         }
     }
-
     private void Drop(object sender, DragEventArgs e) {
         if (e.Source is IControl control &&
             e.Data.Get(string.Empty) is Guid sourceEntityId &&
@@ -53,12 +53,12 @@ public class SceneTreeView : UserControl {
         }
 
         this._isDragging = false;
-        this._dragTarget = Guid.Empty;
-        this.ResetDropTarget(null);
+        this._draggedEntityId = Guid.Empty;
+        this.ResetDropTarget(null, null);
     }
 
     private async void Entity_OnPointerMoved(object sender, PointerEventArgs e) {
-        if (!this._isDragging && this._dragTarget != Guid.Empty && sender is IControl { DataContext: IEntity entity } control && entity.Id == this._dragTarget) {
+        if (!this._isDragging && this._draggedEntityId != Guid.Empty && sender is IControl { DataContext: IEntity entity } control && entity.Id == this._draggedEntityId) {
             this._isDragging = true;
             var dragData = new GenericDataObject(entity.Id, entity.Name);
             await DragDrop.DoDragDrop(e, dragData, DragDropEffects.Move);
@@ -67,15 +67,15 @@ public class SceneTreeView : UserControl {
 
     private void Entity_OnPointerPressed(object sender, PointerPressedEventArgs e) {
         if (e.GetCurrentPoint(this).Properties.PointerUpdateKind == PointerUpdateKind.LeftButtonPressed && sender is IControl { DataContext: IEntity entity }) {
-            this._dragTarget = entity.Id;
+            this._draggedEntityId = entity.Id;
         }
     }
 
     private void Entity_OnPointerReleased(object sender, PointerReleasedEventArgs e) {
         if (e.GetCurrentPoint(this).Properties.PointerUpdateKind == PointerUpdateKind.LeftButtonReleased) {
             this._isDragging = false;
-            this._dragTarget = Guid.Empty;
-            this.ResetDropTarget(null);
+            this._draggedEntityId = Guid.Empty;
+            this.ResetDropTarget(null, null);
         }
     }
 
@@ -83,58 +83,44 @@ public class SceneTreeView : UserControl {
         AvaloniaXamlLoader.Load(this);
     }
 
-    private void ResetDropTarget(IControl newTarget) {
-        var treeViewItem = newTarget?.FindAncestor<TreeViewItem>();
-        if (treeViewItem != this._currentDropTarget) {
-            if (this._currentDropTarget != null) {
-                var borders = this._currentDropTarget.GetVisualDescendants().OfType<Border>().ToList();
-                var topBorder = borders.FirstOrDefault(x => x.Name == "Top");
-                if (topBorder != null) {
-                    topBorder.IsVisible = false;
-                }
-
-                var middleBorder = borders.FirstOrDefault(x => x.Name == "Middle");
-                if (middleBorder != null) {
-                    middleBorder.IsVisible = false;
-                }
-
-                var bottomBorder = borders.FirstOrDefault(x => x.Name == "Bottom");
-                if (bottomBorder != null) {
-                    bottomBorder.IsVisible = false;
-                }
-            }
-
-            this._currentDropTarget = treeViewItem;
+    private void ResetDropTarget(IControl newTarget, DragEventArgs e) {
+        if (this._currentDropTarget != null) {
+            this._currentDropTarget.BorderThickness = EmptyHighlightThickness;
         }
+
+        this._currentDropTarget = newTarget?.FindAncestor<TreeViewItem>();
+
+        if (this._currentDropTarget?.DataContext is IEntity entity && entity.Id == this._draggedEntityId) {
+            this._currentDropTarget = null;
+        }
+        
+        this.SetDropHighlight(e);
     }
 
     private void SetDropHighlight(DragEventArgs e) {
-        if (this._currentDropTarget != null) {
+        // TODO: always keep a border thickness of 1. Make it transparent when empty. Thats it.
+        if (this._currentDropTarget != null && e != null) {
             var (_, y) = e.GetPosition(this._currentDropTarget);
             var (_, height) = this._currentDropTarget.Bounds.Size;
-            var margin = height * 0.1;
-            var borders = this._currentDropTarget.GetVisualDescendants().OfType<Border>().ToList();
-            var topBorder = borders.FirstOrDefault(x => x.Name == "Top");
-            var middleBorder = borders.FirstOrDefault(x => x.Name == "Middle");
-            var bottomBorder = borders.FirstOrDefault(x => x.Name == "Bottom");
-
-            if (topBorder != null && middleBorder != null && bottomBorder != null) {
-                if (y < margin) {
-                    topBorder.IsVisible = true;
-                    middleBorder.IsVisible = false;
-                    bottomBorder.IsVisible = false;
-                }
-                else if (y > height - margin) {
-                    topBorder.IsVisible = false;
-                    middleBorder.IsVisible = false;
-                    bottomBorder.IsVisible = true;
-                }
-                else {
-                    topBorder.IsVisible = false;
-                    middleBorder.IsVisible = true;
-                    bottomBorder.IsVisible = false;
-                }
+            var margin = height * 0.2;
+            if (this._currentDropTarget.DataContext is EntityCollection or IScene) {
+                this._currentDropTarget.BorderThickness = EmptyHighlightThickness;
             }
+            else if (y < margin) {
+                this._currentDropTarget.BorderThickness = TopHighlightThickness;
+            }
+            else if (y > height - margin) {
+                this._currentDropTarget.BorderThickness = BottomHighlightThickness;
+            }
+            else {
+                this._currentDropTarget.BorderThickness = EmptyHighlightThickness;
+            }
+        }
+    }
+
+    private void TreeView_OnLostFocus(object sender, RoutedEventArgs e) {
+        if (this._currentDropTarget != null) {
+            this.ResetDropTarget(null, null);
         }
     }
 }
