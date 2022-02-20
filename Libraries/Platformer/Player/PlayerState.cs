d5dffd44cf55013b4dd95492be6f8c471de358fc
Brett Story <brettmstory@gmail.com>
2022-02-20 12:34:01 -0600

    Initial platformer

---
+++ Libraries/Platformer/Player/PlayerState.cs
@@ -0,0 +1,10 @@
+﻿namespace Macabresoft.Macabre2D.Libraries.Platformer.Player; 
+
+public enum PlayerAnimationKind {
+    Idle,
+    Walking,
+    Running,
+    Sliding,
+    Jumping,
+    Falling
+}
