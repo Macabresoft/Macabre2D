d2ab2981020799500b583b1a6e32e7547e931bd0
Brett Story <brettmstory@gmail.com>
2022-03-13 09:29:13 -0500

    PlatformerCamera has an offset

--- Libraries/Platformer/Rendering/PlatformerCamera.cs
+++ Libraries/Platformer/Rendering/PlatformerCamera.cs
@@ -10,6 +10,7 @@ using Microsoft.Xna.Framework;
 /// </summary>
 public class PlatformerCamera : Camera {
     private Vector2 _distanceAllowedFromParent = Vector2.Zero;
+    private Vector2 _focusOffset = Vector2.Zero;
 
     /// <summary>
     /// Gets or sets the distance allowed from the player.
@@ -18,6 +19,18 @@ public class PlatformerCamera : Camera {
     public Vector2 DistanceAllowedFromParent {
         get => this._distanceAllowedFromParent;
         set => this.Set(ref this._distanceAllowedFromParent, value);
+    }
+
+    /// <summary>
+    /// Gets or sets the focus offset.
+    /// </summary>
+    /// <remarks>
+    /// If this value is non zero, it essentially offsets where the center of this camera is for operations following its owner.
+    /// </remarks>
+    [DataMember]
+    public Vector2 FocusOffset {
+        get => this._focusOffset;
+        set => this.Set(ref this._focusOffset, value);
     }
 
     /// <inheritdoc />
@@ -46,30 +59,30 @@ public class PlatformerCamera : Camera {
 
     private void UpdatePosition() {
         if (this.DistanceAllowedFromParent == Vector2.Zero) {
-            this.LocalPosition = this.Parent.Transform.Position;
+            this.LocalPosition = this.Parent.Transform.Position + this.FocusOffset;
         }
         else if (this.Parent is ITransformable parent) {
             var (parentX, parentY) = parent.Transform.Position;
-            var x = this.LocalPosition.X;
-            var horizontalDistance = parentX - this.LocalPosition.X;
+            var x = this.LocalPosition.X + this.FocusOffset.X;
+            var horizontalDistance = parentX - x;
             if (horizontalDistance > this.DistanceAllowedFromParent.X) {
-                x = parentX - this.DistanceAllowedFromParent.X;
+                x = parentX - this.DistanceAllowedFromParent.X + this.FocusOffset.X;
             }
             else if (horizontalDistance < -this.DistanceAllowedFromParent.X) {
-                x = parentX + this.DistanceAllowedFromParent.X;
+                x = parentX + this.DistanceAllowedFromParent.X + this.FocusOffset.X;
             }
 
-            var y = this.LocalPosition.Y;
-            var verticalDistance = parentY - this.LocalPosition.Y;
+            var y = this.LocalPosition.Y + this.FocusOffset.Y;
+            var verticalDistance = parentY - y;
 
             if (verticalDistance > this.DistanceAllowedFromParent.Y) {
-                y = parentY - this.DistanceAllowedFromParent.Y;
+                y = parentY - this.DistanceAllowedFromParent.Y + this.FocusOffset.X;
             }
             else if (verticalDistance < -this.DistanceAllowedFromParent.Y) {
-                y = parentY + this.DistanceAllowedFromParent.Y;
+                y = parentY + this.DistanceAllowedFromParent.Y + this.FocusOffset.X;
             }
 
-            this.LocalPosition = new Vector2(x, y);
+            this.LocalPosition = new Vector2(x, y) - this.FocusOffset;
         }
     }
 }
