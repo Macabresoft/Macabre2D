02cf9bfe857b6d4cfb35f29771062fd61d47162f
Brett Story <brettmstory@gmail.com>
2022-04-11 19:18:06 -0500

    Can edit colors via hex code

--- UI/Common/Controls/ValueEditors/ColorEditor.axaml.cs
+++ UI/Common/Controls/ValueEditors/ColorEditor.axaml.cs
@@ -1,7 +1,9 @@
 namespace Macabresoft.Macabre2D.UI.Common;
 
+using System;
+using System.Globalization;
 using Avalonia;
-using Avalonia.Data;
+using Avalonia.Interactivity;
 using Avalonia.LogicalTree;
 using Avalonia.Markup.Xaml;
 using Avalonia.Threading;
@@ -28,24 +30,24 @@ public class ColorEditor : ValueEditorControl<Color> {
             editor => editor.GreenValue,
             (editor, value) => editor.GreenValue = value);
 
-    public static readonly DirectProperty<ColorEditor, byte> RedValueProperty =
-        AvaloniaProperty.RegisterDirect<ColorEditor, byte>(
-            nameof(RedValue),
-            editor => editor.RedValue,
-            (editor, value) => editor.RedValue = value);
-    
     public static readonly DirectProperty<ColorEditor, string> HexValueProperty =
         AvaloniaProperty.RegisterDirect<ColorEditor, string>(
             nameof(HexValue),
             editor => editor.HexValue,
             (editor, value) => editor.HexValue = value);
 
+    public static readonly DirectProperty<ColorEditor, byte> RedValueProperty =
+        AvaloniaProperty.RegisterDirect<ColorEditor, byte>(
+            nameof(RedValue),
+            editor => editor.RedValue,
+            (editor, value) => editor.RedValue = value);
+
     private byte _alphaValue;
     private byte _blueValue;
     private byte _greenValue;
-    private byte _redValue;
     private string _hexValue;
     private bool _isUpdating;
+    private byte _redValue;
 
     public ColorEditor() : this(null) {
     }
@@ -70,14 +72,14 @@ public class ColorEditor : ValueEditorControl<Color> {
         set => this.SetAndRaise(GreenValueProperty, ref this._greenValue, value);
     }
 
-    public byte RedValue {
-        get => this._redValue;
-        set => this.SetAndRaise(RedValueProperty, ref this._redValue, value);
-    }
-    
     public string HexValue {
         get => this._hexValue;
         set => this.SetAndRaise(HexValueProperty, ref this._hexValue, value);
+    }
+
+    public byte RedValue {
+        get => this._redValue;
+        set => this.SetAndRaise(RedValueProperty, ref this._redValue, value);
     }
 
     protected override void OnAttachedToLogicalTree(LogicalTreeAttachmentEventArgs e) {
@@ -108,9 +110,12 @@ public class ColorEditor : ValueEditorControl<Color> {
             case nameof(this.AlphaValue):
                 Dispatcher.UIThread.Post(() => this.SetValue(ValueProperty, new Color(this.Value.R, this.Value.G, this.Value.B, this.AlphaValue)));
                 break;
-            case nameof(this.HexValue):
-                Dispatcher.UIThread.Post(this.UpdateValueFromHex);
-                break;
+        }
+    }
+
+    private void HexValueTextBox_OnLostFocus(object sender, RoutedEventArgs e) {
+        if (!string.Equals(this.Value.ToHex(), this.HexValue, StringComparison.OrdinalIgnoreCase)) {
+            Dispatcher.UIThread.Post(this.UpdateValueFromHex);
         }
     }
 
@@ -135,6 +140,29 @@ public class ColorEditor : ValueEditorControl<Color> {
     }
 
     private void UpdateValueFromHex() {
-        // TODO: update it
+        var hexValue = this.HexValue;
+        if (hexValue.StartsWith("#")) {
+            hexValue = hexValue.Replace("#", string.Empty);
+        }
+
+        if (hexValue.Length == 3) {
+            hexValue = $"{hexValue[0]}{hexValue[0]}{hexValue[1]}{hexValue[1]}{hexValue[2]}{hexValue[2]}";
+        }
+
+        if (hexValue.Length == 6) {
+            try {
+                var r = byte.Parse($"{hexValue[0]}{hexValue[1]}", NumberStyles.HexNumber);
+                var g = byte.Parse($"{hexValue[2]}{hexValue[3]}", NumberStyles.HexNumber);
+                var b = byte.Parse($"{hexValue[4]}{hexValue[5]}", NumberStyles.HexNumber);
+                var color = new Color(r, g, b, this.Value.A);
+                Dispatcher.UIThread.Post(() => this.Value = color);
+            }
+            catch {
+                Dispatcher.UIThread.Post(() => this.HexValue = this.Value.ToHex());
+            }
+        }
+        else {
+            Dispatcher.UIThread.Post(() => this.HexValue = this.Value.ToHex());
+        }
     }
 }
