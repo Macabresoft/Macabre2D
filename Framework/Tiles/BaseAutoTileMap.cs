52ef8a6b0a194852678c18fe5ec0fee12ee47b9d
Brett Story <brettmstory@gmail.com>
2022-10-05 16:44:04 -0500

    Removing debug statement

--- Framework/Tiles/BaseAutoTileMap.cs
+++ Framework/Tiles/BaseAutoTileMap.cs
@@ -61,10 +61,6 @@ public abstract class BaseAutoTileMap : RenderableTileMap {
     public override void Render(FrameTime frameTime, BoundingArea viewBoundingArea) {
         if (this.SpriteBatch is { } spriteBatch && this.TileSetReference.PackagedAsset is { } tileSet && this.TileSetReference.Asset is { } spriteSheet) {
             foreach (var (activeTile, tileIndex) in this._activeTileToIndex) {
-                if (activeTile.X == -49) {
-                    Debug.WriteLine("BREAK");
-                }
-                
                 var boundingArea = this.GetTileBoundingArea(activeTile);
                 if (boundingArea.Overlaps(viewBoundingArea) && tileSet.TryGetSpriteIndex(tileIndex, out var spriteIndex) && (this.Scene.BoundingArea.IsEmpty || boundingArea.OverlapsExclusive(this.Scene.BoundingArea))) {
                     spriteBatch.Draw(
