66ede0d42c02f5b83bb01a31f5d3c9fb5c930733
Brett Story <brettmstory@gmail.com>
2023-01-07 14:21:14 -0600

    Can edit scene references

---
+++ Framework/Hierarchy/Assets/SceneReference.cs
@@ -0,0 +1,7 @@
+﻿namespace Macabresoft.Macabre2D.Framework; 
+
+/// <summary>
+/// Reference to a <see cref="SceneAsset" />.
+/// </summary>
+public class SceneReference : AssetReference<SceneAsset, Scene> {
+}
