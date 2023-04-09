4fd63a17e833e35591ae8f7e09efc0799ee30d31
Brett Story <brettmstory@gmail.com>
2023-04-09 08:05:38 -0500

    No longer using the default audio settings

--- Framework/Audio/AudioPlayer.cs
+++ Framework/Audio/AudioPlayer.cs
@@ -65,7 +65,7 @@ public sealed class AudioPlayer : Entity {
     public override void Initialize(IScene scene, IEntity parent) {
         base.Initialize(scene, parent);
 
-        this._instance = this.AudioClipReference.InitializeAndGetInstance(this.Scene.Assets, this.Game.Project.Settings.DefaultAudioSettings, this.Volume, this.Pan, this.Pitch, this.ShouldLoop);
+        this._instance = this.AudioClipReference.InitializeAndGetInstance(this.Scene.Assets, this.Game.AudioSettings, this.Volume, this.Pan, this.Pitch, this.ShouldLoop);
         this.AudioClipReference.PropertyChanged += this.AudioClipReference_PropertyChanged;
 
         if (this.ShouldLoop && this.IsEnabled && !BaseGame.IsDesignMode) {
@@ -120,7 +120,7 @@ public sealed class AudioPlayer : Entity {
         if (e.PropertyName == nameof(this.AudioClipReference.Asset)) {
             if (this.AudioClipReference.Asset is { } audioClip) {
                 this._instance.Dispose();
-                this._instance = audioClip.GetInstance(this.Game.Project.Settings.DefaultAudioSettings, this.Volume, this.Pan, this.Pitch, this.ShouldLoop);
+                this._instance = audioClip.GetInstance(this.Game.AudioSettings, this.Volume, this.Pan, this.Pitch, this.ShouldLoop);
             }
         }
     }
