6f79dd7f56161d792a91ad33171603acf7031be6
Brett Story <brettmstory@gmail.com>
2023-04-15 15:06:04 -0500

    Updated loop reference to allow a generic version

--- Framework/Loops/LoopReference.cs
+++ Framework/Loops/LoopReference.cs
@@ -4,13 +4,56 @@ using System;
 using Macabresoft.Core;
 
 /// <summary>
+/// Base class for loop references.
+/// </summary>
+public abstract class LoopReference : PropertyChangedNotifier {
+    private Guid _loopId;
+
+    /// <summary>
+    /// Gets or sets the loop identifier.
+    /// </summary>
+    public Guid LoopId {
+        get => this._loopId;
+        set {
+            if (this._loopId != value) {
+                this._loopId = value;
+                this.ResetLoop();
+                this.RaisePropertyChanged();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Gets an untyped version of the loop.
+    /// </summary>
+    public ILoop? UntypedLoop { get; protected set; }
+
+    /// <summary>
+    /// Gets the scene.
+    /// </summary>
+    protected IScene Scene { get; private set; } = Framework.Scene.Empty;
+
+    /// <summary>
+    /// Initializes this instance.
+    /// </summary>
+    /// <param name="scene">The scene.</param>
+    public void Initialize(IScene scene) {
+        this.Scene = scene;
+        this.ResetLoop();
+    }
+
+    /// <summary>
+    /// Sets the loop.
+    /// </summary>
+    protected abstract void ResetLoop();
+}
+
+/// <summary>
 /// A reference to a loop using an identifier and type for serialization purposes.
 /// </summary>
 /// <typeparam name="TLoop">The type of loop.</typeparam>
-public class LoopReference<TLoop> : PropertyChangedNotifier where TLoop : class, ILoop {
+public class LoopReference<TLoop> : LoopReference where TLoop : class, ILoop {
     private TLoop? _loop;
-    private Guid _loopId;
-    private IScene _scene = Scene.Empty;
 
     /// <summary>
     /// Gets the loop.
@@ -20,33 +63,15 @@ public class LoopReference<TLoop> : PropertyChangedNotifier where TLoop : class,
         private set => this.Set(ref this._loop, value);
     }
 
-    /// <summary>
-    /// Gets or sets the loop identifier.
-    /// </summary>
-    public Guid LoopId {
-        get => this._loopId;
-        set {
-            if (this.Set(ref this._loopId, value)) {
-                this.SetLoop();
-            }
-        }
-    }
-
-    /// <summary>
-    /// Initializes this instance.
-    /// </summary>
-    /// <param name="scene">The scene.</param>
-    public void Initialize(IScene scene) {
-        this._scene = scene;
-        this.SetLoop();
-    }
-
-    private void SetLoop() {
-        if (this._loopId == Guid.Empty || Scene.IsNullOrEmpty(this._scene)) {
+    /// <inheritdoc />
+    protected override void ResetLoop() {
+        if (this.LoopId == Guid.Empty || Framework.Scene.IsNullOrEmpty(this.Scene)) {
             this.Loop = null;
         }
         else {
-            this.Loop = this._scene.FindLoop<TLoop>(this._loopId);
+            this.Loop = this.Scene.FindLoop<TLoop>(this.LoopId);
         }
+
+        this.UntypedLoop = this.Loop;
     }
 }
