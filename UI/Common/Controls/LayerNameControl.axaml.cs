bc1e78a108f24ccd636dede9d6e50fe943a66f17
Brett Story <brettmstory@gmail.com>
2023-04-22 19:28:21 -0500

    LayerSettingsEditor now uses XAML

---
+++ UI/Common/Controls/LayerNameControl.axaml.cs
@@ -0,0 +1,130 @@
+﻿namespace Macabresoft.Macabre2D.UI.Common;
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.Markup.Xaml;
+using Macabresoft.AvaloniaEx;
+using Macabresoft.Macabre2D.Framework;
+
+public class LayerNameControl : UserControl {
+    public static readonly DirectProperty<LayerNameControl, string> LayerNameProperty =
+        AvaloniaProperty.RegisterDirect<LayerNameControl, string>(
+            nameof(LayerName),
+            editor => editor.LayerName,
+            (editor, value) => editor.LayerName = value,
+            defaultBindingMode: BindingMode.TwoWay);
+
+    public static readonly StyledProperty<Layers> LayerProperty =
+        AvaloniaProperty.Register<LayerNameControl, Layers>(
+            nameof(Layer),
+            defaultBindingMode: BindingMode.OneTime,
+            notifying: OnSettingsOrLayerChanging);
+
+    public static readonly StyledProperty<LayerSettings> LayerSettingsProperty =
+        AvaloniaProperty.Register<LayerNameControl, LayerSettings>(
+            nameof(LayerSettings),
+            defaultBindingMode: BindingMode.OneTime,
+            notifying: OnSettingsOrLayerChanging);
+
+    public static readonly DirectProperty<LayerNameControl, bool> IsLayerEnabledProperty =
+        AvaloniaProperty.RegisterDirect<LayerNameControl, bool>(
+            nameof(IsLayerEnabled),
+            editor => editor.IsLayerEnabled,
+            (editor, value) => editor.IsLayerEnabled = value,
+            defaultBindingMode: BindingMode.TwoWay);
+
+
+    private readonly IUndoService _undoService;
+
+    private bool _isLayerEnabled;
+    private string _layerName;
+
+    public LayerNameControl() : this(Resolver.Resolve<IUndoService>()) {
+    }
+
+    public LayerNameControl(IUndoService undoService) {
+        this._undoService = undoService;
+        this.InitializeComponent();
+    }
+
+    public bool IsLayerEnabled {
+        get => this._isLayerEnabled;
+        set {
+            if (value != this._isLayerEnabled && this.LayerSettings is { } layerSettings) {
+                var originalValue = this.LayerSettings.IsLayerEnabled(this.Layer);
+                this._undoService.Do(() => {
+                        this.SetAndRaise(IsLayerEnabledProperty, ref this._isLayerEnabled, value);
+                        if (value) {
+                            layerSettings.EnableLayers(this.Layer);
+                        }
+                        else {
+                            layerSettings.DisableLayers(this.Layer);
+                        }
+                    },
+                    () => {
+                        this.SetAndRaise(IsLayerEnabledProperty, ref this._isLayerEnabled, originalValue);
+                        if (originalValue) {
+                            layerSettings.EnableLayers(this.Layer);
+                        }
+                        else {
+                            layerSettings.DisableLayers(this.Layer);
+                        }
+                    });
+            }
+        }
+    }
+
+    public Layers Layer {
+        get => this.GetValue(LayerProperty);
+        set => this.SetValue(LayerProperty, value);
+    }
+
+    public string LayerName {
+        get => this._layerName;
+        set {
+            if (value != this._layerName && this.LayerSettings is { } layerSettings) {
+                var originalValue = layerSettings.GetName(this.Layer);
+                this._undoService.Do(() => {
+                        this.SetAndRaise(LayerNameProperty, ref this._layerName, value);
+                        layerSettings.SetName(this.Layer, value);
+                    },
+                    () => {
+                        this.SetAndRaise(LayerNameProperty, ref this._layerName, originalValue);
+                        layerSettings.SetName(this.Layer, originalValue);
+                    });
+            }
+        }
+    }
+
+    public LayerSettings LayerSettings {
+        get => this.GetValue(LayerSettingsProperty);
+        set => this.SetValue(LayerSettingsProperty, value);
+    }
+
+    private void InitializeComponent() {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    private static void OnSettingsOrLayerChanging(IAvaloniaObject control, bool isBeforeChange) {
+        if (control is LayerNameControl layerNameControl) {
+            if (!isBeforeChange) {
+                layerNameControl.Reset();
+            }
+        }
+    }
+
+    private void RaisePropertyChanged<T>(AvaloniaProperty<T> property, T value) {
+        this.RaisePropertyChanged(property, Optional<T>.Empty, new BindingValue<T>(value));
+    }
+
+    private void Reset() {
+        if (this.LayerSettings is { } layerSettings && this.Layer != Layers.None) {
+            this._isLayerEnabled = layerSettings.IsLayerEnabled(this.Layer);
+            this._layerName = layerSettings.GetName(this.Layer);
+            this.RaisePropertyChanged(IsLayerEnabledProperty, this._isLayerEnabled);
+            this.RaisePropertyChanged(LayerNameProperty, this._layerName);
+            this.IsEnabled = this.Layer != Layers.Default;
+        }
+    }
+}
--- UI/Common/Controls/ValueEditors/Framework/LayerSettingsEditor.axaml.cs
+++ UI/Common/Controls/ValueEditors/Framework/LayerSettingsEditor.axaml.cs
@@ -1,193 +1,28 @@
 namespace Macabresoft.Macabre2D.UI.Common;
 
-using System;
-using System.Linq;
-using Avalonia.Controls;
-using Avalonia.Input;
-using Avalonia.Interactivity;
-using Avalonia.LogicalTree;
 using Avalonia.Markup.Xaml;
-using Macabresoft.AvaloniaEx;
 using Macabresoft.Macabre2D.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Unity;
 
 public class LayerSettingsEditor : ValueEditorControl<LayerSettings> {
-    private readonly IUndoService _undoService;
-
-    public LayerSettingsEditor() : this(null, Resolver.Resolve<IUndoService>()) {
+    public LayerSettingsEditor() : this(null) {
     }
 
     [InjectionConstructor]
-    public LayerSettingsEditor(ValueControlDependencies dependencies, IUndoService undoService) : base(dependencies) {
-        this._undoService = undoService;
+    public LayerSettingsEditor(ValueControlDependencies dependencies) : base(dependencies) {
+        var layers = Enum.GetValues<Layers>().ToList();
+        layers.Remove(Layers.None);
+        this.AvailableLayers = layers;
+
         this.InitializeComponent();
     }
 
-    protected override void OnAttachedToLogicalTree(LogicalTreeAttachmentEventArgs e) {
-        base.OnAttachedToLogicalTree(e);
-        this.ConstructControl();
-    }
-
-    protected override void OnDetachedFromLogicalTree(LogicalTreeAttachmentEventArgs e) {
-        base.OnDetachedFromLogicalTree(e);
-        this.UnsubscribeFromEvents();
-    }
-
-    private void CheckBox_Toggled(object sender, RoutedEventArgs e) {
-        if (this.Value != null && sender is CheckBox { Tag: Layers layer } checkBox) {
-            this.ToggleLayer(checkBox, layer);
-        }
-    }
-
-    private void ConstructControl() {
-        if (this.Value == null) {
-            return;
-        }
-
-        var grid = this.LogicalChildren.OfType<Grid>().First();
-
-        var rowValues = Enum.GetValues<Layers>().ToList();
-        rowValues.Remove(Layers.None);
-
-        for (var i = 0; i <= rowValues.Count + 1; i++) {
-            grid.RowDefinitions.Add(new RowDefinition(GridLength.Auto));
-        }
-
-        grid.ColumnDefinitions.Add(new ColumnDefinition(GridLength.Auto));
-        grid.ColumnDefinitions.Add(new ColumnDefinition(GridLength.Auto));
-        grid.ColumnDefinitions.Add(new ColumnDefinition(GridLength.Star));
-
-        for (var row = 0; row < rowValues.Count; row++) {
-            var layer = rowValues[row];
-            var label = new TextBlock {
-                Text = layer.ToString(),
-                [Grid.RowProperty] = row,
-                [Grid.ColumnProperty] = 0
-            };
-
-            label.Classes.Add("Label");
-            grid.Children.Add(label);
-
-            var checkBox = new CheckBox {
-                IsChecked = this.Value.IsLayerEnabled(layer),
-                IsEnabled = layer != Layers.Default,
-                Tag = layer,
-                [Grid.RowProperty] = row,
-                [Grid.ColumnProperty] = 1
-            };
-
-            checkBox.Checked += this.CheckBox_Toggled;
-            checkBox.Unchecked += this.CheckBox_Toggled;
-
-            grid.Children.Add(checkBox);
-
-            var textBox = new TextBox {
-                Text = this.Value.GetName(layer),
-                Tag = layer,
-                IsReadOnly = layer == Layers.Default,
-                [Grid.RowProperty] = row,
-                [Grid.ColumnProperty] = 2
-            };
-
-            textBox.LostFocus += this.LayerTextBox_LostFocus;
-            textBox.KeyUp += this.LayerTextBox_KeyUp;
-            grid.Children.Add(textBox);
-        }
-    }
-
-    private void HandleTextBoxKey(TextBox textBox, Layers layer, KeyEventArgs e) {
-        if (this.Value == null) {
-            return;
-        }
-
-        if (e.Key == Key.Enter) {
-            this.TryUpdateLayerName(textBox, layer);
-        }
-        else if (e.Key == Key.Escape) {
-            var originalName = this.Value.GetName(layer);
-            textBox.Text = originalName;
-        }
-    }
+    public IReadOnlyCollection<Layers> AvailableLayers { get; }
 
     private void InitializeComponent() {
         AvaloniaXamlLoader.Load(this);
-    }
-
-    private void LayerTextBox_KeyUp(object sender, KeyEventArgs e) {
-        if (this.Value != null && sender is TextBox { Tag: Layers layer } textBox) {
-            this.HandleTextBoxKey(textBox, layer, e);
-        }
-    }
-
-    private void LayerTextBox_LostFocus(object sender, RoutedEventArgs e) {
-        if (this.Value != null && sender is TextBox { Tag: Layers layer } textBox) {
-            this.TryUpdateLayerName(textBox, layer);
-        }
-    }
-
-    private void ToggleLayer(CheckBox checkBox, Layers layer) {
-        if (this.Value == null) {
-            return;
-        }
-
-        if (checkBox.IsChecked == true) {
-            if (!this.Value.IsLayerEnabled(layer)) {
-                this._undoService.Do(() =>
-                {
-                    this.Value.EnableLayers(layer);
-                    checkBox.IsChecked = this.Value.IsLayerEnabled(layer);
-                }, () =>
-                {
-                    this.Value.DisableLayers(layer);
-                    checkBox.IsChecked = this.Value.IsLayerEnabled(layer);
-                });
-            }
-        }
-        else {
-            if (this.Value.IsLayerEnabled(layer)) {
-                this._undoService.Do(() =>
-                {
-                    this.Value.DisableLayers(layer);
-                    checkBox.IsChecked = this.Value.IsLayerEnabled(layer);
-                }, () =>
-                {
-                    this.Value.EnableLayers(layer);
-                    checkBox.IsChecked = this.Value.IsLayerEnabled(layer);
-                });
-            }
-        }
-    }
-
-    private void TryUpdateLayerName(TextBox textBox, Layers layer) {
-        if (this.Value == null) {
-            return;
-        }
-
-        var originalName = this.Value.GetName(layer);
-        var updatedName = textBox.Text;
-
-        if (originalName != updatedName && this.Value.SetName(layer, updatedName)) {
-            this._undoService.Do(() =>
-            {
-                this.Value.SetName(layer, updatedName);
-                textBox.Text = updatedName;
-            }, () =>
-            {
-                this.Value.SetName(layer, originalName);
-                textBox.Text = originalName;
-            });
-        }
-    }
-
-    private void UnsubscribeFromEvents() {
-        foreach (var textBox in this.LogicalChildren.OfType<TextBox>()) {
-            textBox.LostFocus -= this.LayerTextBox_LostFocus;
-            textBox.KeyUp -= this.LayerTextBox_KeyUp;
-        }
-
-        foreach (var checkBox in this.LogicalChildren.OfType<CheckBox>()) {
-            checkBox.Checked -= this.CheckBox_Toggled;
-            checkBox.Unchecked -= this.CheckBox_Toggled;
-        }
     }
 }
