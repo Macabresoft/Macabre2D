f0e73f11b20540c74dcecd4ccee8b570dfa62717
Brett Story <brettmstory@gmail.com>
2023-05-07 08:24:08 -0500

    SpriteSheetReferenceEditor

--- UI/Editor/Controls/ValueEditors/BaseSpriteSheetAssetReferenceEditor.cs
+++ UI/Editor/Controls/ValueEditors/BaseSpriteSheetAssetReferenceEditor.cs
@@ -1,40 +1,20 @@
 ﻿namespace Macabresoft.Macabre2D.UI.Editor;
 
-using System;
-using System.IO;
 using System.Threading.Tasks;
-using Avalonia;
-using Avalonia.Media.Imaging;
 using Macabresoft.AvaloniaEx;
 using Macabresoft.Macabre2D.Framework;
 using Macabresoft.Macabre2D.UI.Common;
 
-public class BaseSpriteSheetAssetReferenceEditor<TAssetReference, TPackagedAsset> : BaseAssetReferenceEditor<TAssetReference, SpriteSheet>
-    where TAssetReference : SpriteSheetAssetReference<TPackagedAsset>, IAssetReference<SpriteSheet>
+public class BaseSpriteSheetAssetReferenceEditor<TAssetReference, TPackagedAsset> : BaseSpriteSheetReferenceEditor<TAssetReference>
+    where TAssetReference : Framework.SpriteSheetAssetReference<TPackagedAsset>, IAssetReference<SpriteSheet>
     where TPackagedAsset : SpriteSheetMember {
-    public static readonly DirectProperty<BaseSpriteSheetAssetReferenceEditor<TAssetReference, TPackagedAsset>, Bitmap> BitmapProperty =
-        AvaloniaProperty.RegisterDirect<BaseSpriteSheetAssetReferenceEditor<TAssetReference, TPackagedAsset>, Bitmap>(
-            nameof(Bitmap),
-            editor => editor.Bitmap);
-
-    private readonly IFileSystemService _fileSystem;
-    private readonly IPathService _pathService;
-    private Bitmap _bitmap;
-
     public BaseSpriteSheetAssetReferenceEditor(
         ValueControlDependencies dependencies,
         IAssetManager assetManager,
         ILocalDialogService dialogService,
         IFileSystemService fileSystem,
         IPathService pathService,
-        IUndoService undoService) : base(dependencies, assetManager, dialogService, undoService) {
-        this._fileSystem = fileSystem;
-        this._pathService = pathService;
-    }
-
-    public Bitmap Bitmap {
-        get => this._bitmap;
-        private set => this.SetAndRaise(BitmapProperty, ref this._bitmap, value);
+        IUndoService undoService) : base(dependencies, assetManager, dialogService, fileSystem, pathService, undoService) {
     }
 
     protected override void Clear() {
@@ -49,28 +29,6 @@ public class BaseSpriteSheetAssetReferenceEditor<TAssetReference, TPackagedAsset
                     this.Value.LoadAsset(asset);
                     this.Value.PackagedAssetId = packagedAssetId;
                 });
-        }
-    }
-
-    protected override void ResetPath() {
-        base.ResetPath();
-
-        this.Bitmap = null;
-
-        if (this.AssetManager != null &&
-            this.Value?.Asset != null &&
-            this.Value.ContentId != Guid.Empty &&
-            this.AssetManager.TryGetMetadata(this.Value.ContentId, out var metadata) &&
-            metadata != null) {
-            var fileName = $"{metadata.GetContentPath()}{metadata.ContentFileExtension}";
-            var fullPath = Path.Combine(this._pathService.ContentDirectoryPath, fileName);
-
-            if (this._fileSystem.DoesFileExist(fullPath)) {
-                var bitmap = new Bitmap(fullPath);
-                if (bitmap.PixelSize != PixelSize.Empty) {
-                    this.Bitmap = bitmap;
-                }
-            }
         }
     }
 
---
+++ UI/Editor/Controls/ValueEditors/BaseSpriteSheetReferenceEditor.cs
@@ -0,0 +1,58 @@
+﻿namespace Macabresoft.Macabre2D.UI.Editor;
+
+using System;
+using System.IO;
+using Avalonia;
+using Avalonia.Media.Imaging;
+using Macabresoft.AvaloniaEx;
+using Macabresoft.Macabre2D.Framework;
+using Macabresoft.Macabre2D.UI.Common;
+
+public class BaseSpriteSheetReferenceEditor<TAssetReference> : BaseAssetReferenceEditor<TAssetReference, SpriteSheet> where TAssetReference : class, IAssetReference<SpriteSheet> {
+    public static readonly DirectProperty<BaseSpriteSheetReferenceEditor<TAssetReference>, Bitmap> BitmapProperty =
+        AvaloniaProperty.RegisterDirect<BaseSpriteSheetReferenceEditor<TAssetReference>, Bitmap>(
+            nameof(Bitmap),
+            editor => editor.Bitmap);
+
+    private readonly IFileSystemService _fileSystem;
+    private readonly IPathService _pathService;
+    private Bitmap _bitmap;
+
+    public BaseSpriteSheetReferenceEditor(
+        ValueControlDependencies dependencies,
+        IAssetManager assetManager,
+        ILocalDialogService dialogService,
+        IFileSystemService fileSystem,
+        IPathService pathService,
+        IUndoService undoService) : base(dependencies, assetManager, dialogService, undoService) {
+        this._fileSystem = fileSystem;
+        this._pathService = pathService;
+    }
+
+    public Bitmap Bitmap {
+        get => this._bitmap;
+        private set => this.SetAndRaise(BitmapProperty, ref this._bitmap, value);
+    }
+
+    protected override void ResetPath() {
+        base.ResetPath();
+
+        this.Bitmap = null;
+
+        if (this.AssetManager != null &&
+            this.Value?.Asset != null &&
+            this.Value.ContentId != Guid.Empty &&
+            this.AssetManager.TryGetMetadata(this.Value.ContentId, out var metadata) &&
+            metadata != null) {
+            var fileName = $"{metadata.GetContentPath()}{metadata.ContentFileExtension}";
+            var fullPath = Path.Combine(this._pathService.ContentDirectoryPath, fileName);
+
+            if (this._fileSystem.DoesFileExist(fullPath)) {
+                var bitmap = new Bitmap(fullPath);
+                if (bitmap.PixelSize != PixelSize.Empty) {
+                    this.Bitmap = bitmap;
+                }
+            }
+        }
+    }
+}
