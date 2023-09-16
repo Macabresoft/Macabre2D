7b83ea8d7748735b298f6ea79f80f558994869d9
Brett Story <brettmstory@gmail.com>
2023-09-16 08:50:49 -0500

    Start of user settings

--- Framework/Utility/UserSettings.cs
+++ Framework/Utility/UserSettings.cs
@@ -1,7 +1,9 @@
 ﻿namespace Macabresoft.Macabre2D.Framework;
 
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.Serialization;
+using Newtonsoft.Json;
 
 /// <summary>
 /// User customizable settings.
@@ -13,6 +15,9 @@ public class UserSettings : VersionedData {
     /// The settings file name.
     /// </summary>
     public const string FileName = "UserSettings.m2d";
+
+    [JsonProperty(ObjectCreationHandling = ObjectCreationHandling.Replace)]
+    private readonly Dictionary<string, bool> _boolSettings = new();
 
     /// <summary>
     /// Initializes a new instance of the <see cref="UserSettings" /> class.
@@ -56,4 +61,31 @@ public class UserSettings : VersionedData {
     /// Gets the input bindings.
     /// </summary>
     public InputBindings InputBindings { get; }
+
+    /// <summary>
+    /// Removes a setting by name.
+    /// </summary>
+    /// <param name="setting">The setting's name.</param>
+    public void RemoveSetting(string setting) {
+        this._boolSettings.Remove(setting);
+    }
+
+    /// <summary>
+    /// Attempts to get a <see cref="bool" /> setting if possible.
+    /// </summary>
+    /// <param name="setting">The setting's name.</param>
+    /// <param name="value">The value.</param>
+    /// <returns>A value indicating whether or not the setting was found.</returns>
+    public bool TryGetSetting(string setting, out bool value) {
+        return this._boolSettings.TryGetValue(setting, out value);
+    }
+
+    /// <summary>
+    /// Updates a <see cref="bool" /> setting.
+    /// </summary>
+    /// <param name="setting">The setting's name.</param>
+    /// <param name="value">The value of the setting.</param>
+    public void UpdateSetting(string setting, bool value) {
+        this._boolSettings[setting] = value;
+    }
 }
