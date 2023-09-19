3dc446fe0dca05775b735da8a24f7d65e6fb289f
Brett Story <brettmstory@gmail.com>
2023-09-19 16:36:23 -0500

    Fixed BoolSetting compile error

--- Framework/Settings/NamedSettings/BoolSetting.cs
+++ Framework/Settings/NamedSettings/BoolSetting.cs
@@ -1,11 +1,15 @@
 ﻿namespace Macabresoft.Macabre2D.Framework;
 
+using System;
 using System.Runtime.Serialization;
 
 /// <summary>
 /// A <see cref="NamedSetting" /> for <see cref="bool" /> values.
 /// </summary>
 public class BoolSetting : NamedSetting {
+    /// <inheritdoc />
+    public override Type ValueType => typeof(bool);
+
     /// <summary>
     /// Gets or sets the value of this setting.
     /// </summary>
