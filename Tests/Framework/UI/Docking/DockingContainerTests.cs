9646a0a991cbd9ce0eba81cebc9ebf9d8b6bd09f
Brett Story <brettmstory@gmail.com>
2023-10-07 16:38:25 -0500

    RequestRearrange unit test

--- Tests/Framework/UI/Docking/DockingContainerTests.cs
+++ Tests/Framework/UI/Docking/DockingContainerTests.cs
@@ -31,9 +31,45 @@ public class DockingContainerTests {
                 offset,
                 new Vector2(containerSize),
                 new Vector2(panelSize),
-                out var panel);
+                out var panel,
+                out _);
 
             panels.Add(panel);
+        }
+
+        using (new AssertionScope()) {
+            foreach (var panel in panels) {
+                panel.BoundingArea.Minimum.Should().BeEquivalentTo(new Vector2(resultX, resultY));
+            }
+        }
+    }
+    
+    [Test]
+    [Category("Unit Tests")]
+    [TestCase(DockLocation.Center, 10f, 5f, 2.5f, 2.5f)]
+    [TestCase(DockLocation.Left, 10f, 5f, 0f, 2.5f)]
+    [TestCase(DockLocation.Top, 10f, 5f, 2.5f, 5f)]
+    [TestCase(DockLocation.Right, 10f, 5f, 5f, 2.5f)]
+    [TestCase(DockLocation.Bottom, 10f, 5f, 2.5f, 0f)]
+    [TestCase(DockLocation.BottomLeft, 10f, 5f, 0f, 0f)]
+    [TestCase(DockLocation.BottomRight, 10f, 5f, 5f, 0f)]
+    [TestCase(DockLocation.TopLeft, 10f, 5f, 0f, 5f)]
+    [TestCase(DockLocation.TopRight, 10f, 5f, 5f, 5f)]
+    public static void RequestRearrange_ShouldAlignProperly(DockLocation dockLocation, float containerSize, float panelSize, float resultX, float resultY) {
+        var panels = new List<DockablePanel>();
+
+        foreach (var offset in OffsetTypes) {
+            CreateDependencies(
+                dockLocation,
+                offset,
+                new Vector2(containerSize),
+                new Vector2(panelSize),
+                out var panel,
+                out var container);
+
+            panels.Add(panel);
+            panel.Move(new Vector2(-100f, -100f)); // Requires rearrangement.
+            container.RequestRearrange(panel);
         }
 
         using (new AssertionScope()) {
@@ -50,7 +86,8 @@ public class DockingContainerTests {
         PixelOffsetType boundingOffsetType,
         Vector2 containerSize,
         Vector2 dockableSize,
-        out DockablePanel panel) {
+        out DockablePanel panel,
+        out DockingContainer container) {
         var game = Substitute.For<IGame>();
         var scene = Substitute.For<IScene>();
         var project = Substitute.For<IGameProject>();
@@ -64,13 +101,12 @@ public class DockingContainerTests {
             BoundingArea = new BoundingArea(Vector2.Zero, containerSize)
         };
 
-        var container = boundable.AddChild<DockingContainer>();
+        container = boundable.AddChild<DockingContainer>();
         panel = container.AddChild<DockablePanel>();
         panel.OffsetOptions.OffsetType = boundingOffsetType;
         panel.Location = dockLocation;
         panel.Width = dockableSize.X;
         panel.Height = dockableSize.Y;
-
         boundable.Initialize(scene, scene);
     }
 }
