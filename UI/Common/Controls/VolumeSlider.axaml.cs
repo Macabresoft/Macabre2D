dbeb82928a698e64b5fa1d20fd373f2af7e7da8d
Brett Story <brettmstory@gmail.com>
2023-10-28 13:18:59 -0500

    VolumeSlider now works

--- UI/Common/Controls/VolumeSlider.axaml.cs
+++ UI/Common/Controls/VolumeSlider.axaml.cs
@@ -11,18 +11,32 @@ using Avalonia.Interactivity;
 public partial class VolumeSlider : UserControl, IObserver<AvaloniaPropertyChangedEventArgs<float>> {
     private const float Tolerance = 0.001f;
 
+    public static readonly DirectProperty<VolumeSlider, float> ValueDisplayProperty =
+        AvaloniaProperty.RegisterDirect<VolumeSlider, float>(
+            nameof(ValueDisplay),
+            editor => editor.ValueDisplay,
+            (editor, value) => editor.ValueDisplay = value);
+
     public static readonly StyledProperty<float> ValueProperty =
         AvaloniaProperty.Register<VolumeSlider, float>(nameof(Value), defaultBindingMode: BindingMode.TwoWay);
 
     private IDisposable _pointerReleaseDispose;
 
+    private float _valueDisplay;
+
     public VolumeSlider() {
+        ValueProperty.Changed.Subscribe(this);
         this.InitializeComponent();
     }
 
     public float Value {
         get => this.GetValue(ValueProperty);
         set => this.SetValue(ValueProperty, value);
+    }
+
+    public float ValueDisplay {
+        get => this._valueDisplay;
+        set => this.SetAndRaise(ValueDisplayProperty, ref this._valueDisplay, value);
     }
 
     public void OnCompleted() {
@@ -32,8 +46,8 @@ public partial class VolumeSlider : UserControl, IObserver<AvaloniaPropertyChang
     }
 
     public void OnNext(AvaloniaPropertyChangedEventArgs<float> value) {
-        if (this.Content is Slider slider) {
-            slider.Value = this.Value;
+        if (value.Sender == this && value.NewValue.HasValue && Math.Abs(value.NewValue.Value - this.ValueDisplay) > Tolerance) {
+            this.ValueDisplay = value.NewValue.Value;
         }
     }
 
@@ -48,8 +62,14 @@ public partial class VolumeSlider : UserControl, IObserver<AvaloniaPropertyChang
     }
 
     private void OnPointerReleased(object sender, PointerReleasedEventArgs e) {
-        if (this.Content is Slider slider && Math.Abs(this.Value - slider.Value) > Tolerance) {
-            this.Value = (float)slider.Value;
+        if (Math.Abs(this.Value - this.ValueDisplay) > Tolerance && sender == this.Content) {
+            this.Value = this.ValueDisplay;
+        }
+    }
+
+    private void Slider_OnKeyUp(object sender, KeyEventArgs e) {
+        if (Math.Abs(this.Value - this.ValueDisplay) > Tolerance && sender == this.Content) {
+            this.Value = this.ValueDisplay;
         }
     }
 }
