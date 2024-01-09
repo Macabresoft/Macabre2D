96f1a9db109e0bd4710baa692884f242594af8b7
Brett Story <brettmstory@gmail.com>
2024-01-09 16:06:45 -0600

    Cleanup

--- Framework/Serialization/ISaveDataManager.cs
+++ Framework/Serialization/ISaveDataManager.cs
@@ -1,13 +1,14 @@
 namespace Macabresoft.Macabre2D.Framework;
 
 /// <summary>
-/// Interface for a save data mananger.
+/// Interface for a save data manager.
 /// </summary>
 public interface ISaveDataManager {
     /// <summary>
     /// Deletes the specified save data.
     /// </summary>
     /// <param name="fileName">Name of the file.</param>
+    /// <param name="projectName">The project name.</param>
     void Delete(string fileName, string projectName);
 
     /// <summary>
@@ -21,6 +22,7 @@ public interface ISaveDataManager {
     /// </summary>
     /// <typeparam name="T">Versioned data.</typeparam>
     /// <param name="fileName">Name of the file.</param>
+    /// <param name="projectName">The project name.</param>
     /// <param name="saveData">The save data.</param>
     void Save<T>(string fileName, string projectName, T saveData) where T : IVersionedData;
 
@@ -29,6 +31,7 @@ public interface ISaveDataManager {
     /// </summary>
     /// <typeparam name="T">The type of data.</typeparam>
     /// <param name="fileName">Name of the file.</param>
+    /// <param name="projectName">The project name.</param>
     /// <param name="data">The loaded data.</param>
     /// <returns>A value indicating whether or not the data was found.</returns>
     bool TryLoad<T>(string fileName, string projectName, out T? data) where T : class, IVersionedData;
