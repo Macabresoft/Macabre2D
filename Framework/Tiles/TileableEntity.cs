5ed9fbb1074435b9e7a45ed7a11af49290ba7a78
Brett Story <brettmstory@gmail.com>
2024-07-21 09:23:33 -0500

    Moving CardinalDirections to its own file

--- Framework/Tiles/TileableEntity.cs
+++ Framework/Tiles/TileableEntity.cs
@@ -7,23 +7,6 @@ using Macabresoft.Core;
 using Microsoft.Xna.Framework;
 
 /// <summary>
-/// Represents four directions from a single tile.
-/// </summary>
-[Flags]
-public enum CardinalDirections : byte {
-    None = 0,
-    North = 1 << 0,
-    West = 1 << 1,
-    East = 1 << 2,
-    South = 1 << 3,
-    NorthEast = North | East,
-    NorthWest = North | West,
-    SouthEast = South | East,
-    SouthWest = South | West,
-    All = North | West | East | South
-}
-
-/// <summary>
 /// A tileable entity.
 /// <see cref="ITileableEntity" />
 /// </summary>
---
+++ Framework/Utility/CardinalDirections.cs
@@ -0,0 +1,20 @@
+﻿namespace Macabresoft.Macabre2D.Framework;
+
+using System;
+
+/// <summary>
+/// Represents four directions from a single tile.
+/// </summary>
+[Flags]
+public enum CardinalDirections : byte {
+    None = 0,
+    North = 1 << 0,
+    West = 1 << 1,
+    East = 1 << 2,
+    South = 1 << 3,
+    NorthEast = North | East,
+    NorthWest = North | West,
+    SouthEast = South | East,
+    SouthWest = South | West,
+    All = North | West | East | South
+}
