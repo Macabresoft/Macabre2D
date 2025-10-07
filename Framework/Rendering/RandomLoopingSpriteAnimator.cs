300734f7f77a6603fbc9c2608d3abbd588188309
macabrett <brett@macabresoft.com>
2025-10-07 16:47:22 -0500

    Can set a first animation on the RandomLoopingSpriteAnimator

--- Framework/Rendering/RandomLoopingSpriteAnimator.cs
+++ Framework/Rendering/RandomLoopingSpriteAnimator.cs
@@ -14,13 +14,25 @@ public class RandomLoopingSpriteAnimator : BaseSpriteAnimator {
     private QueueableSpriteAnimation? _currentAnimation;
 
     /// <summary>
+    /// Gets a reference to the first animation to be played.
+    /// </summary>
+    /// <remarks>
+    /// If no first animation is selected, a random animation from <see cref="SpriteSheetReference" /> will be used.
+    /// </remarks>
+    [DataMember(Order = 10, Name = "First Animation")]
+    public SpriteAnimationReference FirstAnimationReference { get; } = new();
+
+    /// <summary>
+    /// Gets or sets a value indicating whether animations should be pulled at random or in the order they appear in their collection.
+    /// </summary>
+    [DataMember]
+    public bool IsRandom { get; set; } = true;
+
+    /// <summary>
     /// Gets the sprite sheet reference.
     /// </summary>
     [DataMember(Order = 10, Name = "Sprite Sheet")]
     public SpriteSheetReference SpriteSheetReference { get; } = new();
-
-    [DataMember]
-    public bool IsRandom { get; set; } = true;
 
     /// <summary>
     /// Gets or sets a value indicating whether this should start playing by default.
@@ -38,16 +50,24 @@ public class RandomLoopingSpriteAnimator : BaseSpriteAnimator {
     public override void Initialize(IScene scene, IEntity parent) {
         base.Initialize(scene, parent);
 
-        if (this.TryResetAnimations()) {
+        this.TryResetAnimations();
+
+        if (this.TrySetFirstAnimation() || this.TryAssignNewAnimation()) {
             this.TryStart();
         }
 
         this.SpriteSheetReference.PropertyChanged += this.AnimationReference_PropertyChanged;
     }
 
+    public override void Stop() {
+        this.TrySetFirstAnimation();
+        base.Stop();
+    }
+
     /// <inheritdoc />
     protected override IEnumerable<IAssetReference> GetAssetReferences() {
         yield return this.SpriteSheetReference;
+        yield return this.FirstAnimationReference;
     }
 
     /// <inheritdoc />
@@ -69,7 +89,9 @@ public class RandomLoopingSpriteAnimator : BaseSpriteAnimator {
 
     private void AnimationReference_PropertyChanged(object? sender, PropertyChangedEventArgs e) {
         if (e.PropertyName is nameof(SpriteAnimationReference.Asset) or nameof(SpriteAnimationReference.PackagedAsset) or nameof(Framework.SpriteSheet.SpriteSize)) {
-            if (this.TryResetAnimations()) {
+            this.TryResetAnimations();
+
+            if (this.TryAssignNewAnimation()) {
                 this.TryStart();
             }
         }
@@ -100,7 +122,7 @@ public class RandomLoopingSpriteAnimator : BaseSpriteAnimator {
         return false;
     }
 
-    private bool TryResetAnimations() {
+    private void TryResetAnimations() {
         this._availableAnimations.Clear();
 
         if (this.SpriteSheetReference.Asset is { } spriteSheet) {
@@ -108,8 +130,16 @@ public class RandomLoopingSpriteAnimator : BaseSpriteAnimator {
                 this._availableAnimations.Add(new QueueableSpriteAnimation(animation, false));
             }
         }
+    }
 
-        return this.TryAssignNewAnimation();
+    private bool TrySetFirstAnimation() {
+        if (this.FirstAnimationReference.PackagedAsset is { } firstAnimation) {
+            this._currentAnimation = new QueueableSpriteAnimation(firstAnimation, false);
+            this._currentAnimation.Reset();
+            return true;
+        }
+
+        return false;
     }
 
     private void TryStart() {
