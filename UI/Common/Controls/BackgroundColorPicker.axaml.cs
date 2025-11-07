04629926d9b044b187a3353d94614fd565a2869f
macabrett <brett@macabresoft.com>
2025-11-07 16:34:18 -0600

    Common control for background color picker

---
+++ UI/Common/Controls/BackgroundColorPicker.axaml.cs
@@ -0,0 +1,26 @@
+﻿namespace Macabresoft.Macabre2D.UI.Common;
+
+using System.Collections.Generic;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data;
+using Macabresoft.Macabre2D.Project.Common;
+using Microsoft.Xna.Framework;
+
+public partial class BackgroundColorPicker : UserControl {
+
+    public static readonly StyledProperty<Color> BackgroundColorProperty = AvaloniaProperty.Register<BackgroundColorPicker, Color>(
+        nameof(BackgroundColor),
+        Color.Transparent,
+        defaultBindingMode: BindingMode.TwoWay);
+
+
+    public BackgroundColorPicker() {
+        this.InitializeComponent();
+    }
+
+    public Color BackgroundColor {
+        get => this.GetValue(BackgroundColorProperty);
+        set => this.SetValue(BackgroundColorProperty, value);
+    }
+}
