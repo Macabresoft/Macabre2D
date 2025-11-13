bc37636a41f8fa8b891eed75cd9ee97a5f7865a5
macabrett <brett@macabresoft.com>
2025-11-13 16:17:45 -0600

    Customize the number of frames that can be held by a RandomFrameLoopingSpriteAnimator

--- Framework/Rendering/RandomFrameLoopingSpriteAnimator.cs
+++ Framework/Rendering/RandomFrameLoopingSpriteAnimator.cs
@@ -1,18 +1,41 @@
 ﻿namespace Macabresoft.Macabre2D.Framework;
 
 using System;
+using System.Runtime.Serialization;
 
 /// <summary>
 /// A looping sprite animator which picks a random sprite out of the animation for each new frame.
 /// </summary>
 public class RandomFrameLoopingSpriteAnimator : LoopingSpriteAnimator {
+    private const byte MinimumFramesToHold = 1;
     private readonly Random _random = new();
+    private byte _framesHeld;
+    private byte _numberOfFramesToHold = 1;
+
+    /// <summary>
+    /// Gets or sets the number of frames to hold onto a given sprite.
+    /// </summary>
+    [DataMember]
+    public byte NumberOfFramesToHold {
+        get => this._numberOfFramesToHold;
+        set => this._numberOfFramesToHold = Math.Max(value, MinimumFramesToHold);
+    }
+
+    /// <inheritdoc />
+    public override void Initialize(IScene scene, IEntity parent) {
+        base.Initialize(scene, parent);
+        this._framesHeld = 0;
+    }
 
     /// <inheritdoc />
     public override void NextFrame() {
-        if (this.GetCurrentAnimation() is { } animation) {
+        if (this._framesHeld >= this.NumberOfFramesToHold && this.GetCurrentAnimation() is { } animation) {
             var step = this._random.Next(0, animation.Animation.Steps.Count);
             animation.Reset(step, 0, 0d);
+            this._framesHeld = 0;
+        }
+        else {
+            this._framesHeld++;
         }
     }
 }
