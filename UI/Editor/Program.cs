a20d3decac8cbd3c41686f05aadad46ee534cf49
macabrett <brett@macabresoft.com>
2025-11-23 08:37:06 -0600

    Upgrade NuGet packages

--- UI/Editor/Program.cs
+++ UI/Editor/Program.cs
@@ -2,16 +2,15 @@ namespace Macabresoft.Macabre2D.UI.Editor;
 
 using System;
 using Avalonia;
-using Avalonia.ReactiveUI;
+using ReactiveUI.Avalonia;
 
 internal static class Program {
     // Avalonia configuration, don't remove; also used by visual designer.
-    public static AppBuilder BuildAvaloniaApp() {
-        return AppBuilder.Configure<App>()
+    public static AppBuilder BuildAvaloniaApp() =>
+        AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .LogToTrace()
             .UseReactiveUI();
-    }
 
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
