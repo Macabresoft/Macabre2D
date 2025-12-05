c147d0fffeaa43f419d8233c343fbe090f935d0c
macabrett <brett@macabresoft.com>
2025-12-05 15:55:52 -0600

    Caption on physics material reference editor to showcase the current bounce and friction

---
+++ UI/Common/Converters/PhysicsMaterialToCaptionConverter.cs
@@ -0,0 +1,27 @@
+﻿namespace Macabresoft.Macabre2D.UI.Common;
+
+using System;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+using Macabresoft.Macabre2D.Framework;
+
+/// <summary>
+/// Converts from a <see cref="PhysicsMaterial" /> to a caption.
+/// </summary>
+public class PhysicsMaterialToCaptionConverter : IValueConverter {
+    /// <summary>
+    /// Gets the instance.
+    /// </summary>
+    public static IValueConverter Instance { get; } = new PhysicsMaterialToCaptionConverter();
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+        if (value is PhysicsMaterial material) {
+            return $"Bounce: {material.Bounce:F2}, Friction {material.Friction:F2}";
+        }
+
+        return AvaloniaProperty.UnsetValue;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
