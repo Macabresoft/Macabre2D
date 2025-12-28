1d438a0c47a892734ca2c98a35b1ad479176b55f
macabrett <brett@macabresoft.com>
2025-12-28 14:50:32 -0600

    New routine system

---
+++ Framework/Systems/Routines/Routine.cs
@@ -0,0 +1,37 @@
+﻿namespace Macabresoft.Macabre2D.Framework.Routines;
+
+using System;
+
+/// <summary>
+/// Represents a routine that can be run over multiple frames.
+/// </summary>
+public sealed class Routine {
+    private readonly Func<FrameTime, bool> _routine;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Routine" /> class.
+    /// </summary>
+    /// <param name="routine">The routine. This takes in a <see cref="FrameTime" /> and returns a <see cref="bool" /> indicating whether it is finished running.</param>
+    public Routine(Func<FrameTime, bool> routine) {
+        this._routine = routine;
+    }
+
+    /// <summary>
+    /// Gets a value indicating whether this routine has been canceled.
+    /// </summary>
+    public bool IsCanceled { get; private set; }
+
+    /// <summary>
+    /// Cancels this routine as of its next scheduled run.
+    /// </summary>
+    public void Cancel() {
+        this.IsCanceled = true;
+    }
+
+    /// <summary>
+    /// Runs the routine and returns a value indicating whether it is complete.
+    /// </summary>
+    /// <param name="frameTime">The frame time.</param>
+    /// <returns>A value indicating whether the routine was finished.</returns>
+    public bool Run(FrameTime frameTime) => this._routine(frameTime);
+}
---
+++ Framework/Systems/Routines/RoutineSystem.cs
@@ -0,0 +1,54 @@
+﻿namespace Macabresoft.Macabre2D.Framework.Routines;
+
+using System;
+using System.Collections.Generic;
+using Macabresoft.Core;
+
+/// <summary>
+/// A system which runs routines over multiple frames.
+/// </summary>
+public class RoutineSystem : GameSystem, IUpdateSystem {
+    private readonly List<Routine> _routines = [];
+
+    /// <inheritdoc />
+    public event EventHandler? ShouldUpdateChanged;
+
+    /// <inheritdoc />
+    public UpdateSystemKind Kind => UpdateSystemKind.Update;
+
+    /// <inheritdoc />
+    public bool ShouldUpdate {
+        get;
+        private set {
+            if (value != field) {
+                field = value;
+                this.ShouldUpdateChanged.SafeInvoke(this);
+            }
+        }
+    } = false;
+
+    /// <summary>
+    /// Runs the routine until it is finished.
+    /// </summary>
+    /// <param name="routine">The routine.</param>
+    public void Run(Routine routine) {
+        this._routines.Add(routine);
+        this.ShouldUpdate = true;
+    }
+
+    /// <inheritdoc />
+    public void Update(FrameTime frameTime, InputState inputState) {
+        var removed = new List<Routine>();
+        foreach (var routine in this._routines) {
+            if (routine.IsCanceled || routine.Run(frameTime)) {
+                removed.Add(routine);
+            }
+        }
+
+        foreach (var routine in removed) {
+            this._routines.Remove(routine);
+        }
+
+        this.ShouldUpdate = this._routines.Count != 0;
+    }
+}
