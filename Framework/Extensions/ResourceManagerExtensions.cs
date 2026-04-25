f11dde41e849adaece57175082f08b98a7981b2f
macabrett <brett@macabrett.com>
2026-04-25 15:52:06 -0500

    Extension block in ResourceManagerExtensions.cs

--- Framework/Extensions/ResourceManagerExtensions.cs
+++ Framework/Extensions/ResourceManagerExtensions.cs
@@ -9,29 +9,30 @@ using Macabre2D.Project.Common;
 /// Extension methods for <see cref="ResourceManager" />.
 /// </summary>
 public static class ResourceManagerExtensions {
-    /// <summary>
-    /// Tries to get a resource string from a <see cref="ResourceManager" />.
-    /// </summary>
     /// <param name="manager">The manager.</param>
-    /// <param name="resourceName">The resource name.</param>
-    /// <param name="resource">The resource.</param>
-    /// <returns>A value indicating whether the resource was found.</returns>
-    public static bool TryGetString(this ResourceManager manager, string resourceName, [NotNullWhen(true)] out string? resource) {
-        resource = manager.GetString(resourceName, Resources.Culture);
-        return resource != null;
-    }
+    extension(ResourceManager manager) {
+        /// <summary>
+        /// Tries to get a resource string from a <see cref="ResourceManager" />.
+        /// </summary>
+        /// <param name="resourceName">The resource name.</param>
+        /// <param name="resource">The resource.</param>
+        /// <returns>A value indicating whether the resource was found.</returns>
+        public bool TryGetString(string resourceName, [NotNullWhen(true)] out string? resource) {
+            resource = manager.GetString(resourceName, Resources.Culture);
+            return resource != null;
+        }
 
-    /// <summary>
-    /// Tries to get a resource string from a <see cref="ResourceManager" />.
-    /// </summary>
-    /// <param name="manager">The manager.</param>
-    /// <param name="resourceName">The resource name.</param>
-    /// <param name="culture">The culture.</param>
-    /// <param name="resource">The resource.</param>
-    /// <returns>A value indicating whether the resource was found.</returns>
-    public static bool TryGetString(this ResourceManager manager, string resourceName, ResourceCulture culture, [NotNullWhen(true)] out string? resource) {
-        var cultureInfo = CultureInfo.GetCultureInfo(culture.ToCultureName());
-        resource = manager.GetString(resourceName, cultureInfo);
-        return resource != null;
+        /// <summary>
+        /// Tries to get a resource string from a <see cref="ResourceManager" />.
+        /// </summary>
+        /// <param name="resourceName">The resource name.</param>
+        /// <param name="culture">The culture.</param>
+        /// <param name="resource">The resource.</param>
+        /// <returns>A value indicating whether the resource was found.</returns>
+        public bool TryGetString(string resourceName, ResourceCulture culture, [NotNullWhen(true)] out string? resource) {
+            var cultureInfo = CultureInfo.GetCultureInfo(culture.ToCultureName());
+            resource = manager.GetString(resourceName, cultureInfo);
+            return resource != null;
+        }
     }
 }
