2fa6a2feabcb707cbc51abc1159220a058353750
macabrett <brett@macabrett.com>
2026-06-14 12:49:23 -0500

    Cleanup

--- Framework/Rendering/Text/TextAreaRenderer.cs
+++ Framework/Rendering/Text/TextAreaRenderer.cs
@@ -25,7 +25,7 @@ public class TextAreaRenderer : BaseSpriteSheetFontRenderer, ITextRenderer {
     public override event EventHandler? BoundingAreaChanged;
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="TextLineRenderer" /> class.
+    /// Initializes a new instance of the <see cref="TextAreaRenderer" /> class.
     /// </summary>
     public TextAreaRenderer() {
         this._boundingArea = new ResettableLazy<BoundingArea>(this.CreateBoundingArea);
