e380369c5632a62cf27bd31851db2eef106a26e3
macabrett <brett@macabrett.com>
2026-07-03 09:21:05 -0500

    Clean up licenses

--- Framework/Utility/LicenseHelper.cs
+++ Framework/Utility/LicenseHelper.cs
@@ -9,11 +9,10 @@ public static class LicenseHelper {
     /// <summary>
     /// Gets all license definitions.
     /// </summary>
-    public static IReadOnlyCollection<LicenseDefinition> Definitions { get; } = new LicenseDefinition[] {
+    public static IReadOnlyCollection<LicenseDefinition> Definitions { get; } = [
         new("Macabre2D", Licenses.Macabre2D),
         new("Macabresoft.Core", Licenses.Macabresoft_Core),
         new("MonoGame", Licenses.MonoGame),
-        new("Mono.Xna", Licenses.Mono_Xna),
         new("AvaloniaUI", Licenses.AvaloniaUI),
         new("Public Sans", Licenses.Public_Sans),
         new("Fluent UI System Icons (Microsoft)", Licenses.Fluent_UI_System_Icons),
@@ -24,17 +23,8 @@ public static class LicenseHelper {
         new("NUnit", Licenses.NUnit),
         new("NUnit 3 Test Adapter", Licenses.NUnit3TestAdapter),
         new("Unity", Licenses.Unity),
-        new("AssimpNet", Licenses.AssimpNet),
-        new("Castle.Core", Licenses.Castle_Core),
-        new("DynamicData", Licenses.DynamicData),
-        new("HalfBuzzSharp", Licenses.SkiaSharp),
-        new("JetBRains.Annotations", Licenses.Jetbrains_Annotations),
         new("Microsoft .NET Library", Licenses.Microsoft__NET_Library),
         new("NuGet", Licenses.NuGet),
-        new("ReactiveUI", Licenses.ReactiveUI),
-        new("SkiaSharp", Licenses.SkiaSharp),
-        new("Splat", Licenses.Splat),
-        new("System.Reactive", Licenses.System_Reactive),
-        new("Tmds.DBus", Licenses.Tmds_DBus)
-    };
+        new("ReactiveUI", Licenses.ReactiveUI)
+    ];
 }
